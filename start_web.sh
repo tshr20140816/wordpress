e88756c14b56a1460b294bfde97e2174066a46d2
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-09-26 14:35:59 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -4,4 +4,16 @@ set -x
 
 export TZ=JST-9
 
+if [ ! -v BASIC_USER ]; then
+  echo "Error : BASIC_USER not defined."
+  exit
+fi
+
+if [ ! -v BASIC_PASSWORD ]; then
+  echo "Error : BASIC_PASSWORD not defined."
+  exit
+fi
+
+htpasswd -c -b .htpasswd ${BASIC_USER} ${BASIC_PASSWORD}
+
 vendor/bin/heroku-php-apache2 -C apache.conf www
