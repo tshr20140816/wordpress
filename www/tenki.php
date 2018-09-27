37def4fa8edf666d13ab211ad257c8f4560cf0ab
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-09-28 05:25:03 +0900

    Update tenki.php

--- www/tenki.php
+++ www/tenki.php
@@ -3,8 +3,12 @@
 // $res = file_get_contents('https://rss-weather.yahoo.co.jp/rss/days/' . getenv('LOCATION_NUMBER') . '.xml');
 $rss = simplexml_load_file('https://rss-weather.yahoo.co.jp/rss/days/' . getenv('LOCATION_NUMBER') . '.xml');
 
+$counter = 0;
 foreach($rss->channel->item as $item) {
 
+  if ($counter++ == 7) {
+    break;
+  }
   echo $item->title;
   echo $item->link;
   
