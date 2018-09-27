6de0c2eab2a3f7af79eb688df4c89a699a8aabc7
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-09-28 06:50:58 +0900

    Update tenki.php

--- www/tenki.php
+++ www/tenki.php
@@ -3,7 +3,10 @@
 $res = file_get_contents('https://tenki.jp/week/' . getenv('LOCATION_NUMBER') . '/');
 
 $tmp = explode(getenv('POINT_NAME'), $res);
+$tmp = explode('<td class="forecast-wrap">', $tmp[1]);
 
-error_log($tmp[1]);
+for ($i = 0; $i < 10; $i++) {
+  error_log($tmp[$i]);
+}
 
 ?>
