759337c3d82ea4c9d0746ddb7c15895d0fa80410
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-09-28 06:39:07 +0900

    Update tenki.php

--- www/tenki.php
+++ www/tenki.php
@@ -1,30 +1,8 @@
 <?php
 
-// $res = file_get_contents('https://rss-weather.yahoo.co.jp/rss/days/' . getenv('LOCATION_NUMBER') . '.xml');
-$rss = simplexml_load_file('https://rss-weather.yahoo.co.jp/rss/days/' . getenv('LOCATION_NUMBER') . '.xml');
+$res = file_get_contents('https://tenki.jp/week/ . getenv('LOCATION_NUMBER') . '/');
 
-$counter = 0;
-foreach($rss->channel->item as $item) {
-
-  if ($counter++ == 7) {
-    break;
-  }
-  
-  $url = $item->link;
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, FALSE);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-  curl_setopt($ch, CURLOPT_HEADER, TRUE);
-  $tmp = curl_exec($ch);
-  $info = curl_getinfo($ch);
-  // echo $info['CURLINFO_EFFECTIVE_URL'];
-  error_log($info['CURLINFO_EFFECTIVE_URL']);
-  error_log($info['CURLINFO_REDIRECT_URL']);
-  curl_close($ch);
-  
-  echo $item->title;
-  echo $item->link;
+echo $res;
   
 }
 
