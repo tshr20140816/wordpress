bcf6d5630cd3b95142a9e709bec3231ff38b138f
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-09-28 10:38:11 +0900

    Update tenki.php

--- www/tenki.php
+++ www/tenki.php
@@ -8,7 +8,13 @@ $tmp = explode('<td class="forecast-wrap">', $tmp[1]);
 for ($i = 1; $i < 11; $i++) {
   //error_log(str_replace(' ', '', trim(strip_tags($tmp[$i]))));
   $list = explode("\n", str_replace(' ', '', trim(strip_tags($tmp[$i]))));
-  error_log($list[0] . ' ' . $list[1] . ' ' . $list[2]);
+  $tmp = $list[0];
+  $tmp = str_replace('晴', '☼', $tmp);
+  $tmp = str_replace('曇', '☁', $tmp);
+  $tmp = str_replace('雨', '☂', $tmp);
+  $tmp = str_replace('のち', '→', $tmp);
+  $tmp = str_replace('時々', '|', $tmp);
+  error_log($tmp . ' ' . $list[1] . ' ' . $list[2]);
 }
 
 ?>
