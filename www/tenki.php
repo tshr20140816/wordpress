bcca228c5186b3606d4ec3b6431deb310c92a2f6
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-09-28 10:29:27 +0900

    Update tenki.php

--- www/tenki.php
+++ www/tenki.php
@@ -6,7 +6,7 @@ $tmp = explode(getenv('POINT_NAME'), $res);
 $tmp = explode('<td class="forecast-wrap">', $tmp[1]);
 
 for ($i = 1; $i < 11; $i++) {
-  error_log(trim(strip_tags($tmp[$i])));
+  error_log(' ', '', str_replace(trim(strip_tags($tmp[$i])));
 }
 
 ?>
