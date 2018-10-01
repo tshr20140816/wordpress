0cdff9f16d6e896630c3a89d9cfe127180569680
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-01 13:07:20 +0900

    Update tenki.php

--- www/tenki.php
+++ www/tenki.php
@@ -13,7 +13,7 @@ $tmp = explode(getenv('POINT_NAME'), $res);
 $tmp = explode('<td class="forecast-wrap">', $tmp[1]);
 
 for ($i = 0; $i < 10; $i++) {
-  error_log(date('m/d', strtotime($dt . ' +' . $i . " day")));
+  // error_log(date('m/d', strtotime($dt . ' +' . $i . " day")));
   $list = explode("\n", str_replace(' ', '', trim(strip_tags($tmp[$i + 1]))));
   $tmp2 = $list[0];
   $tmp2 = str_replace('晴', '☼', $tmp2);
@@ -22,7 +22,7 @@ for ($i = 0; $i < 10; $i++) {
   $tmp2 = str_replace('のち', '/', $tmp2);
   $tmp2 = str_replace('時々', '|', $tmp2);
   $tmp2 = str_replace('一時', '|', $tmp2);
-  error_log('+++++ ' . $tmp2 . ' ' . $list[1] . ' ' . $list[2]. ' +++++');
+  error_log('+++++ ' date('m/d', strtotime($dt . ' +' . $i . ' day')) . ' ' . $tmp2 . ' ' . $list[1] . ' ' . $list[2]. ' +++++');
 }
 
 ?>
