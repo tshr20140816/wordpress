1a5dd6ce4b8a070b1b9989971e7188336948db1f
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-02 13:42:52 +0900

    Update tenki.php

--- www/tenki.php
+++ www/tenki.php
@@ -52,8 +52,7 @@ curl_close($ch);
 $params = json_decode($res, TRUE);
 error_log($params['access_token']);
 
-//$res = file_get_contents('https://api.toodledo.com/3/tasks/get.php?access_token=' . $params['access_token'] . '&comp=0&folder=WEATHER');
-$res = file_get_contents('https://api.toodledo.com/3/tasks/get.php?access_token=' . $params['access_token'] . '&comp=0&fields=duedate,tag');
+//$res = file_get_contents('https://api.toodledo.com/3/tasks/get.php?access_token=' . $params['access_token'] . '&comp=0&fields=duedate,tag');
 
 //error_log($res);
 
