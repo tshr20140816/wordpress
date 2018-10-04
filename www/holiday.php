ea5e20b5cb28b00055d03cf6f2cde08b4cb851de
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-04 11:36:48 +0900

    Create holiday.php

---
+++ www/holiday.php
@@ -0,0 +1,12 @@
+<?php
+
+// $url = 'http://calendar-service.net/cal?start_year=2018&start_mon=11&end_year=2020&end_mon=12&year_style=normal&month_style=numeric&wday_style=ja_full&format=csv&holiday_only=1&zero_padding=1';
+$url = 'http://calendar-service.net/cal?start_year=2018&start_mon=11&end_year=2018&end_mon=11&year_style=normal&month_style=numeric&wday_style=ja_full&format=csv&holiday_only=1&zero_padding=1';
+
+$res = file_get_contents($url);
+
+$res = mb_convert_encoding($res, 'UTF-8', 'EUC-JP');
+
+error_log($res);
+
+?>
