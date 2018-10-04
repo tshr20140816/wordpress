eb459538c5f19efd690d0c3468e3342ab6a5a18d
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-04 11:54:38 +0900

    Update holiday.php

--- www/holiday.php
+++ www/holiday.php
@@ -1,7 +1,7 @@
 <?php
 
 // $url = 'http://calendar-service.net/cal?start_year=2018&start_mon=11&end_year=2020&end_mon=12&year_style=normal&month_style=numeric&wday_style=ja_full&format=csv&holiday_only=1&zero_padding=1';
-$url = 'http://calendar-service.net/cal?start_year=2018&start_mon=11&end_year=2018&end_mon=11&year_style=normal&month_style=numeric&wday_style=ja_full&format=csv&holiday_only=1&zero_padding=1';
+$url = 'http://calendar-service.net/cal?start_year=2018&start_mon=11&end_year=2018&end_mon=12&year_style=normal&month_style=numeric&wday_style=ja_full&format=csv&holiday_only=1&zero_padding=1';
 
 $res = file_get_contents($url);
 
@@ -9,8 +9,10 @@ $res = mb_convert_encoding($res, 'UTF-8', 'EUC-JP');
 
 // error_log($res);
 
-$tmp = explode("\n", $res);
-error_log($tmp[1]);
-error_log($tmp[2]);
+$tmp_list = explode("\n", $res);
+
+for ($i = 1; $i < count($tmp_list); $i++) {
+  error_log($tmp_list[$i]);
+}
 
 ?>
