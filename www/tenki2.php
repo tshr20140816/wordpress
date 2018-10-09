36d182e04c493904b021fbc0eb6762eaf2568aa6
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-09 12:36:09 +0900

    Update tenki2.php

--- www/tenki2.php
+++ www/tenki2.php
@@ -1,5 +1,9 @@
 <?php
 
+error_log('START');
+
+error_log(getenv('WEATHER_URL') . '/november-weather/' . getenv('WEATHER_SUB_CODE'));
+  
 $res = file_get_contents(getenv('WEATHER_URL') . '/november-weather/' . getenv('WEATHER_SUB_CODE'));
 
 error_log($res);
