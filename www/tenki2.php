0e37469d9870e3a4da70b3eddf675a8aaf8b6bb8
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-09 12:39:22 +0900

    Update tenki2.php

--- www/tenki2.php
+++ www/tenki2.php
@@ -2,10 +2,10 @@
 
 error_log('START');
 
-error_log(getenv('WEATHER_URL') . '/november-weather/' . getenv('WEATHER_SUB_CODE'));
+error_log(getenv('WEATHER_URL') . 'november-weather/' . getenv('WEATHER_SUB_CODE'));
 
-// $res = file_get_contents(getenv('WEATHER_URL') . '/november-weather/' . getenv('WEATHER_SUB_CODE'));
-$res = file_get_contents('https://www.accuweather.com/ja/jp/hiroshima-shi/223955/october-weather/223955?monyr=10/1/2018&view=table');
+$res = file_get_contents(getenv('WEATHER_URL') . 'november-weather/' . getenv('WEATHER_SUB_CODE'));
+// $res = file_get_contents('https://www.accuweather.com/ja/jp/hiroshima-shi/223955/october-weather/223955?monyr=10/1/2018&view=table');
 
 error_log($res);
 
