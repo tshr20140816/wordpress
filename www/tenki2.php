2408cfae078ddad41ffcd0b553db3a34ab70a403
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-09 12:44:28 +0900

    Update tenki2.php

--- www/tenki2.php
+++ www/tenki2.php
@@ -4,9 +4,27 @@ error_log('START');
 
 error_log(getenv('WEATHER_URL') . 'november-weather/' . getenv('WEATHER_SUB_CODE'));
 
-$res = file_get_contents(getenv('WEATHER_URL') . 'november-weather/' . getenv('WEATHER_SUB_CODE'));
-// $res = file_get_contents('https://www.accuweather.com/ja/jp/hiroshima-shi/223955/october-weather/223955?monyr=10/1/2018&view=table');
+$res = get_contents(getenv('WEATHER_URL') . 'november-weather/' . getenv('WEATHER_SUB_CODE'), NULL);
 
 error_log($res);
 
+function get_contents($url_, $options_) {
+  $ch = curl_init();
+  curl_setopt_array($ch, [
+    CURLOPT_URL => $url_,
+    CURLOPT_RETURNTRANSFER => TRUE,
+    CURLOPT_ENCODING => '',
+    CURLOPT_FOLLOWLOCATION => 1,
+    CURLOPT_MAXREDIRS => 3,
+    CURLOPT_SSL_FALSESTART => TRUE,
+    CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1; rv:56.0) Gecko/20100101 Firefox/62.0',
+    ]);
+  if (is_null($options_) == FALSE) {
+    curl_setopt_array($ch, $options_);
+  }
+  $res = curl_exec($ch);
+  curl_close($ch);
+  
+  return $res;
+}
 ?>
