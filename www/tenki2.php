75640723330ee461fb26b656702c98f263e561a7
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-09 13:46:18 +0900

    Update tenki2.php

--- www/tenki2.php
+++ www/tenki2.php
@@ -19,6 +19,10 @@ function get_contents($url_, $options_) {
     CURLOPT_MAXREDIRS => 3,
     CURLOPT_SSL_FALSESTART => TRUE,
     CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1; rv:56.0) Gecko/20100101 Firefox/62.0',
+    CURLOPT_HTTPHEADER => [
+      'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Language:ja,en-us;q=0.7,en;q=0.3',
+      ],
     ]);
   if (is_null($options_) == FALSE) {
     curl_setopt_array($ch, $options_);
