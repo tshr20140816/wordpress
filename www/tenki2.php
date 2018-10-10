7f49105598afc4435dc04b8af25679055bf8d790
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-10 14:13:13 +0900

    Update tenki2.php

--- www/tenki2.php
+++ www/tenki2.php
@@ -79,12 +79,11 @@ $pdo = null;
 
 // Get Tasks
 
-error_log('ACCESS TOKEN : ' . $access_token);
 $res = get_contents('https://api.toodledo.com/3/tasks/get.php?access_token=' . $access_token . '&comp=0&fields=tag', NULL);
 // error_log($res);
 
 $tasks = json_decode($res, TRUE);
-error_log(print_r($tasks, TRUE));
+// error_log(print_r($tasks, TRUE));
 $list_delete_task = [];
 for ($i = 0; $i < count($tasks); $i++) {
   if (array_key_exists('id', $tasks[$i]) && array_key_exists('tag', $tasks[$i])) {
