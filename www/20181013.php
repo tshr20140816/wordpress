7f593a8647e0430bb4e8a190842fac849aee8589
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-13 11:48:05 +0900

    Update 20181013.php

--- www/20181013.php
+++ www/20181013.php
@@ -3,7 +3,7 @@
 for ($i = 0; $i < 60; $i++) {
   $timestamp = strtotime('+' . ($i + 10) . ' days');
   $diff = $timestamp - time();
-  error_log($diff / 60 / 60);
+  error_log($diff / 60 / 60 / 24);
 }
 
 ?>
