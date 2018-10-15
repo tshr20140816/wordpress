aa74080642eb9438090893040878502aa101ca90
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-15 17:26:14 +0900

    Update holiday.php

--- www/holiday.php
+++ www/holiday.php
@@ -86,7 +86,6 @@ $start_m = date('n', strtotime('+2 month'));
 $finish_yyyy = $start_yyyy + 2;
 // $finish_m = 12;
 
-// $url = 'http://calendar-service.net/cal?start_year=2018&start_mon=11&end_year=2020&end_mon=12&year_style=normal&month_style=numeric&wday_style=ja_full&format=csv&holiday_only=1&zero_padding=1';
 $url = 'http://calendar-service.net/cal?start_year=' . $start_yyyy . '&start_mon=' . $start_m . '&end_year=' . $finish_yyyy . '&end_mon=12&year_style=normal&month_style=numeric&wday_style=ja_full&format=csv&holiday_only=1&zero_padding=1';
 
 $res = file_get_contents($url);
@@ -101,7 +100,6 @@ for ($i = 1; $i < count($tmp_list) - 1; $i++) {
   error_log($tmp_list[$i]);
   $tmp = explode(',', $tmp_list[$i]);
   error_log('####+ ' . $tmp[7] . ' (' . $tmp[5] . ') ' . $tmp[0] . '/' . $tmp[1] . '/' . $tmp[2] . ' +####');
-  // $holiday_list[$tmp[0] . $tmp[1] . $tmp[2] . $tmp[7]] = '####+ ' . $tmp[7] . ' (' . $tmp[5] . ') ' . $tmp[0] . '/' . $tmp[1] . '/' . $tmp[2] . ' +####';
   $holiday_list['####+ ' . $tmp[7] . ' (' . $tmp[5] . ') ' . $tmp[0] . '/' . $tmp[1] . '/' . $tmp[2] . ' +####'] = $tmp[0] . $tmp[1] . $tmp[2] . $tmp[7];
 }
 
