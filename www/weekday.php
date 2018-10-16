73d54932d78e84bdcb5e97725b6ffa3c6a6ca860
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-16 16:40:30 +0900

    Update weekday.php

--- www/weekday.php
+++ www/weekday.php
@@ -107,16 +107,16 @@ $tasks = json_decode($res, TRUE);
 //error_log(print_r($tasks, TRUE));
 
 $edit_task_list = [];
-$edit_task_template = '{"id":"__ID__","content":"__CONTENT__"}';
+$edit_task_template = '{"id":"__ID__","context":"__CONTEXT__"}';
 
 for ($i = 0; $i < count($tasks); $i++) {
   if (array_key_exists('id', $tasks[$i])) {
-    $real_content_id = $yobi_list[intval(date('w', $tasks[$i]['duedate']))];
+    $real_context_id = $yobi_list[intval(date('w', $tasks[$i]['duedate']))];
     $task_context_id = $tasks[$i]['context'];
-    if ($task_context_id == '0' || $task_context_id != $real_content_id) {
+    if ($task_context_id == '0' || $task_context_id != $real_context_id) {
       error_log(print_r($tasks[$i], TRUE));
       $tmp = str_replace('__ID__', $tasks[$i]['id'], $edit_task_template);
-      $tmp = str_replace('__CONTENT__', $real_content_id, $tmp);
+      $tmp = str_replace('__CONTEXT__', $real_context_id, $tmp);
       $edit_task_list[] = $tmp;
       break;
     }
@@ -126,7 +126,7 @@ error_log($pid . ' ' . print_r($edit_task_list, TRUE));
 // $edit_task_list = array_slice($edit_task_list, 0, 50);
 
 $tmp = implode(',', $edit_task_list);
-$post_data = ['access_token' => $access_token, 'tasks' => "[${tmp}], fields=content"];
+$post_data = ['access_token' => $access_token, 'tasks' => "[${tmp}]", 'fields' => 'context'];
 
 error_log($pid . ' ' . print_r($post_data, TRUE));
 
