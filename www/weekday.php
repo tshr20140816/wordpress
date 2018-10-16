b1f4a8a228d42435e4d10a52457449bb705f0348
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-16 16:08:05 +0900

    Update weekday.php

--- www/weekday.php
+++ www/weekday.php
@@ -104,7 +104,7 @@ $res = get_contents('https://api.toodledo.com/3/tasks/get.php?access_token=' . $
 // error_log($res);
 
 $tasks = json_decode($res, TRUE);
-// error_log(print_r($tasks, TRUE));
+error_log(print_r($tasks, TRUE));
 
 // $list_marker_task_title = [];
 for ($i = 0; $i < count($tasks); $i++) {
