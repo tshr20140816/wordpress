5fc18ecd41ddf7c6e1ae8efa47ff59a9e6f10b67
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-16 10:04:09 +0900

    Update 20181015.php

--- www/20181015.php
+++ www/20181015.php
@@ -1,10 +1,12 @@
 <?php
 
 $year = date('%Y', '+3 years');
+error_log($year);
 
 for ($i = 0; $i < 1096 - 80; $i++) {
   $timestamp = strtotime('+' . ($i + 80) . ' days');
   $y = date('Y', $timestamp);
+  error_log($y);
   if ($year == $y) {
     break;
   }
