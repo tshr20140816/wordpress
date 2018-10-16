42165a634a6e2452f5e0bc6cb923b8f890c2bf45
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-16 15:48:58 +0900

    Create weekday.php

---
+++ www/weekday.php
@@ -0,0 +1,84 @@
+<?php
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+error_log("${pid} START ${requesturi}");
+
+// Access Token
+
+$connection_info = parse_url(getenv('DATABASE_URL'));
+$pdo = new PDO(
+  "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
+  $connection_info['user'],
+  $connection_info['pass']);
+
+$sql = <<< __HEREDOC__
+SELECT M1.access_token
+      ,M1.refresh_token
+      ,M1.expires_in
+      ,M1.create_time
+      ,M1.update_time
+      ,CASE WHEN LOCALTIMESTAMP < M1.update_time + interval '90 minutes' THEN 0 ELSE 1 END refresh_flag
+  FROM m_authorization M1;
+__HEREDOC__;
+
+$access_token = NULL;
+foreach ($pdo->query($sql) as $row) {
+  $access_token = $row['access_token'];
+  $refresh_token = $row['refresh_token'];
+  $refresh_flag = $row['refresh_flag'];
+}
+
+if ($access_token == NULL) {
+  exit();
+}
+
+if ($refresh_flag == 1) {
+  error_log("${pid} refresh_token : ${refresh_token}");
+  $post_data = ['grant_type' => 'refresh_token', 'refresh_token' => $refresh_token];
+  
+  $res = get_contents(
+    'https://api.toodledo.com/3/account/token.php',
+    [CURLOPT_USERPWD => getenv('TOODLEDO_CLIENTID') . ':' . getenv('TOODLEDO_SECRET'),
+     CURLOPT_POST => TRUE,
+     CURLOPT_POSTFIELDS => http_build_query($post_data),
+    ]);
+  
+  error_log("${pid} token.php RESPONSE : ${res}");
+  $params = json_decode($res, TRUE);
+
+  $sql = <<< __HEREDOC__
+UPDATE m_authorization
+   SET access_token = :b_access_token
+      ,refresh_token = :b_refresh_token
+      ,update_time = LOCALTIMESTAMP;
+__HEREDOC__;
+
+  $statement = $pdo->prepare($sql);
+  $rc = $statement->execute([':b_access_token' => $params['access_token'],
+                             ':b_refresh_token' => $params['refresh_token']]);
+  error_log("${pid} UPDATE RESULT : ${rc}");
+  
+  $access_token = $params['access_token'];
+}
+
+$pdo = null;
+
+// Get Tasks
+
+$res = get_contents('https://api.toodledo.com/3/tasks/get.php?access_token=' . $access_token . '&comp=0&fields=tag', NULL);
+// error_log($res);
+
+$tasks = json_decode($res, TRUE);
+// error_log(print_r($tasks, TRUE));
+$list_marker_task_title = [];
+for ($i = 0; $i < count($tasks); $i++) {
+  if (array_key_exists('id', $tasks[$i]) && array_key_exists('tag', $tasks[$i])) {
+    if ($tasks[$i]['tag'] == 'MARKER') {
+      $list_marker_task_title[$tasks[$i]['title']] = $tasks[$i]['id'];
+    }
+  }
+}
+error_log($pid . ' ' . print_r($list_marker_task_title, TRUE));
+
+?>
