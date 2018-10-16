72eb99ec17f67780592fb722c373a90d3bbb5504
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-16 09:52:45 +0900

    Update 20181015.php

--- www/20181015.php
+++ www/20181015.php
@@ -2,10 +2,16 @@
 
 $year = date('%Y');
 
-for ($i = 0; $i < 30; $i++) {
+for ($i = 0; $i < 731 - 80; $i++) {
   $timestamp = strtotime('+' . ($i + 80) . ' days');
+  $y = date('Y', $timestamp);
+  if ($year + 3 == $y) {
+    break;
+  }
   $d = date('j', $timestamp);
-  error_log($d);
+  if ($d == 1 || $d == 11 || $d == 21) {
+    error_log(date('m/d', $timestamp));
+  }
 }
 
 ?>
