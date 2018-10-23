d92769aa53dab93810d54cf646c1fce9c958366a
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-23 16:05:22 +0900

    Update 20181023.php

--- www/20181023.php
+++ www/20181023.php
@@ -2,7 +2,7 @@
 
 $res = file_get_contents('https://eco.mtk.nao.ac.jp/koyomi/dni/2018/m2011.html');
 
-$tmp = exlode('</tr>', $res);
+$tmp = explode('</tr>', $res);
 
 error_log(print_r($tmp, TRUE));
 
