99eec0eb6740aa76f79f8c6af01d27c64c890578
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-24 13:09:29 +0900

    Update 20181024.php

--- www/20181024.php
+++ www/20181024.php
@@ -1,3 +1,32 @@
 <?php
 
+$post_data = ['from_year' => '2019']
+
+$res = get_contents(
+  'http://www.calc-site.com/calendars/solar_year',
+  [CURLOPT_POST => TRUE,
+   CURLOPT_POSTFIELDS => http_build_query($post_data),
+  ]);
+
+error_log($res);
+
+function get_contents($url_, $options_) {
+  $ch = curl_init();
+  curl_setopt_array($ch, [
+    CURLOPT_URL => $url_,
+    CURLOPT_USERAGENT => getenv('USER_AGENT'),
+    CURLOPT_RETURNTRANSFER => TRUE,
+    CURLOPT_ENCODING => '',
+    CURLOPT_FOLLOWLOCATION => 1,
+    CURLOPT_MAXREDIRS => 3,
+    CURLOPT_SSL_FALSESTART => TRUE,
+    ]);
+  if (is_null($options_) == FALSE) {
+    curl_setopt_array($ch, $options_);
+  }
+  $res = curl_exec($ch);
+  curl_close($ch);
+  
+  return $res;
+}
 ?>
