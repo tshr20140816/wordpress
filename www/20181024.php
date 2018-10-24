01ec9c2d2f95d845aac3b9d5b8319560c6cf7cee
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-24 10:01:42 +0900

    Create 20181024.php

---
+++ www/20181024.php
@@ -0,0 +1,13 @@
+<?php
+
+$res = file_get_contents('https://eco.mtk.nao.ac.jp/koyomi/dni/2018/s1308.html');
+
+$tmp = explode('<table ', $res);
+$tmp = explode('</table>', $tmp[1]);
+$tmp = explode('</tr>', $tmp[0]);
+array_shift($tmp);
+array_pop($tmp);
+
+error_log(print_r($tmp, TRUE));
+
+?>
