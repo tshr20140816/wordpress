47b85a076323377d8caca24de1dd403124265bb8
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-25 13:36:29 +0900

    Update tenki2.php

--- www/tenki2.php
+++ www/tenki2.php
@@ -157,7 +157,7 @@ $res = get_contents('https://api.toodledo.com/3/tasks/get.php?access_token=' . $
 // error_log($res);
 
 $tasks = json_decode($res, TRUE);
-// error_log(print_r($tasks, TRUE));
+error_log($pid . ' $tasks : ' . print_r($tasks, TRUE));
 $list_delete_task = [];
 for ($i = 0; $i < count($tasks); $i++) {
   error_log($pid . ' ' . $i . ' ' . $tasks[$i]);
