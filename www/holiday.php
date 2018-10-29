833fbc4feff826b3e2c90f8dc540647fef5a5c14
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-29 15:45:47 +0900

    Update holiday.php

--- www/holiday.php
+++ www/holiday.php
@@ -92,7 +92,7 @@ for ($i = 0; $i < count($tasks); $i++) {
   if (array_key_exists('duedate', $tasks[$i]) && array_key_exists('folder', $tasks[$i])) {
     if ($tasks[$i]['folder'] == $label_folder_id) {
       $list_label_task[] = date('Ymd', $tasks[$i]['duedate']);
-      error_log($pid . ' ' . date('Y-m-d', $tasks[$i]['duedate']) . ' ' . $tasks[$i]['duedate']);
+      // error_log($pid . ' ' . date('Y-m-d', $tasks[$i]['duedate']) . ' ' . $tasks[$i]['duedate']);
     }
   }
 }
