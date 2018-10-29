0aa853c8e5ad9f519ae0f6fa57924d3995037347
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-29 10:39:15 +0900

    Create 20181029.php

---
+++ www/20181029.php
@@ -0,0 +1,74 @@
+<?php
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+error_log("${pid} START ${requesturi}");
+
+// Access Token
+
+$connection_info = parse_url(getenv('DATABASE_URL'));
+$pdo = new PDO(
+  "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
+  $connection_info['user'],
+  $connection_info['pass']);
+
+$sql = <<< __HEREDOC__
+SELECT M1.access_token
+      ,M1.refresh_token
+      ,M1.expires_in
+      ,M1.create_time
+      ,M1.update_time
+      ,CASE WHEN LOCALTIMESTAMP < M1.update_time + interval '90 minutes' THEN 0 ELSE 1 END refresh_flag
+  FROM m_authorization M1;
+__HEREDOC__;
+
+$access_token = NULL;
+foreach ($pdo->query($sql) as $row) {
+  $access_token = $row['access_token'];
+  $refresh_token = $row['refresh_token'];
+  $refresh_flag = $row['refresh_flag'];
+}
+
+if ($access_token == NULL) {
+  error_log("${pid} ACCESS TOKEN NONE");
+  exit();
+}
+
+// Get Folders
+
+$res = get_contents('https://api.toodledo.com/3/folders/get.php?access_token=' . $access_token, NULL);
+$folders = json_decode($res, TRUE);
+
+$label_folder_id = 0;
+for ($i = 0; $i < count($folders); $i++) {
+  if ($folders[$i]['name'] == 'LABEL') {
+    $label_folder_id = $folders[$i]['id'];
+    error_log($pid . ' LABEL FOLDER ID : ' . $label_folder_id);
+    break;
+  }
+}
+
+// Get Tasks
+
+$res = get_contents('https://api.toodledo.com/3/tasks/get.php?access_token=' . $access_token . '&comp=0&fields=tag', NULL);
+// error_log($res);
+
+$tasks = json_decode($res, TRUE);
+error_log(print_r($tasks, TRUE));
+
+exit;
+
+$list_delete_task = [];
+for ($i = 0; $i < count($tasks); $i++) {
+  if (array_key_exists('id', $tasks[$i])) {
+    if ($tasks[$i]['tag'] == 'WEATHER') {
+      $list_delete_task[] = $tasks[$i]['id'];
+      error_log("${pid} DELETE TARGET TASK ID : " . $tasks[$i]['id']);
+      if (count($list_delete_task) == 50) {
+        break;
+      }
+    }
+  }
+}
+
+?>
