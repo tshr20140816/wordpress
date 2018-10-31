94743e1da24b3b678e357b5c860c8b2fa63b3bdd
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-31 13:58:17 +0900

    Update tenki2.php

--- www/tenki2.php
+++ www/tenki2.php
@@ -211,9 +211,9 @@ error_log($pid . ' $list_sunrise_sunset : ' . print_r($list_sunrise_sunset, TRUE
 // Weather Information 今日の10日後から70日分
 
 $list_base = [];
-for ($i = 0; $i < 9; $i++) {
+for ($i = 0; $i < 11; $i++) {
   $url = 'https://feed43.com/' . getenv('SUB_ADDRESS') . ($i * 5 + 11) . '-' . ($i * 5 + 15) . '.xml';
-  error_log($pid . ' ' . $url);
+  error_log($pid . ' $url : ' . $url);
   $res = get_contents($url, NULL);
   error_log($pid . ' ' . $res);
   foreach (explode("\n", $res) as $one_line) {
