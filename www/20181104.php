1f5a15bde9a1dfe239635c99ec7105bd3bb425a7
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-04 11:03:55 +0900

    Update 20181104.php

--- www/20181104.php
+++ www/20181104.php
@@ -11,11 +11,8 @@ $tmp = explode('<col span=1 align=right>', $res);
 $tmp = explode('</table>', $tmp[1]);
 
 error_log($tmp[0]);
-/*
-<tr bgcolor="#fef0ef"><td width=60><font color="#c00000"><b>3</b> (土)</font></td><td>
-<font color="#c00000">文化の日</font>　×××大混雑9:00～21:00</td></tr>
-*/
-$rc = preg_match_all('/<tr .+?<b>(.+?)<.*?<td>(.*?)<\/td><\/tr>/s', $tmp[0], $matches, PREG_SET_ORDER);
+
+$rc = preg_match_all('/<tr .+?<b>(.+?)<.*?<td(.*?)<\/td><\/tr>/s', $tmp[0], $matches, PREG_SET_ORDER);
 
 error_log(print_r($matches, TRUE));
 ?>
