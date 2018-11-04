d64fcab0c0e5e2d0ac848c00f659c8ce853f8f64
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-04 09:04:30 +0900

    Update MyUtils.php

--- classes/MyUtils.php
+++ classes/MyUtils.php
@@ -2,6 +2,24 @@
 
 class MyUtils
 {
-
+  function get_contents($url_, $options_) {
+    $ch = curl_init();
+    curl_setopt_array($ch, [
+      CURLOPT_URL => $url_,
+      CURLOPT_USERAGENT => getenv('USER_AGENT'),
+      CURLOPT_RETURNTRANSFER => TRUE,
+      CURLOPT_ENCODING => '',
+      CURLOPT_FOLLOWLOCATION => 1,
+      CURLOPT_MAXREDIRS => 3,
+      CURLOPT_SSL_FALSESTART => TRUE,
+      ]);
+    if (is_null($options_) == FALSE) {
+      curl_setopt_array($ch, $options_);
+    }
+    $res = curl_exec($ch);
+    curl_close($ch);
+  
+    return $res;
+  }
 }
 ?>
