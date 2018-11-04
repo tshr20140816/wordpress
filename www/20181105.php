4710eda9344b60c59f1e0fd794a5516964b10f53
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-05 08:56:58 +0900

    Update 20181105.php

--- www/20181105.php
+++ www/20181105.php
@@ -4,10 +4,13 @@ $url = 'https://soccer.yahoo.co.jp/jleague/teams/schedule/129';
 
 $res = file_get_contents($url);
 
-error_log($res);
+// error_log($res);
 
 $rc = preg_match_all('/<td class="textC">(.+?)<\/tr>/s', $res, $matches, PREG_SET_ORDER);
 
-error_log(print_r($matches, TRUE));
+// error_log(print_r($matches, TRUE));
 
+for ($i < 0; $i < count($matches); $i++) {
+  error_log(print_r($matches[$i], TRUE));  
+}
 ?>
