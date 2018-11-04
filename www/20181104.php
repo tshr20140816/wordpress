3620bc24f116a70e20dbc84c30ed0fd4cc8ddf98
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-04 21:02:19 +0900

    Update 20181104.php

--- www/20181104.php
+++ www/20181104.php
@@ -7,10 +7,10 @@ $mu = new MyUtils();
 $y = date('Y');
 $m = date('n');
 
+$list_library = [];
 for ($j = 0; $j < 2; $j++) {
   $url = 'http://www.cf.city.hiroshima.jp/saeki-cs/sche6_park/sche6.cgi?year=' . $y . '&mon=' . $m;
 
-  // $res = file_get_contents($url);
   $res = $mu->get_contents($url, NULL);
   $res = mb_convert_encoding($res, 'UTF-8', 'SJIS');
 
@@ -30,11 +30,12 @@ for ($j = 0; $j < 2; $j++) {
     $tmp = trim($tmp, " \t\n\r\0\t>");
     $tmp = preg_replace('/<font .+?>.+?>/', '', $tmp);
     $tmp = str_replace('　', '', $tmp);
+    $tmp = preg_replace('/bgcolor.+?>/', '', $tmp);
+    $tmp = trim(str_replace('<br>', ' ', $tmp));
     if (strlen($tmp) > 0) {
-      $tmp = preg_replace('/bgcolor.+?>/', '', $tmp);
-      $tmp = trim(str_replace('<br>', ' ', $tmp));
       $timestamp = mktime(0, 0, 0, $m, $matches[$i][1], $y);
-      error_log(date('m/d', $timestamp) . ' 文セ ★ ' . $tmp);
+      $list_library[$timestamp] = date('m/d', $timestamp) . ' 文セ ★ ' . $tmp;
+      // error_log(date('m/d', $timestamp) . ' 文セ ★ ' . $tmp);
     }
   }
   if ($m == 12) {
@@ -44,5 +45,6 @@ for ($j = 0; $j < 2; $j++) {
     $m++;
   }
 }
+error_log(print_r($list_library, TRUE));
 
 ?>
