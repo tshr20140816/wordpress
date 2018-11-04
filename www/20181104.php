e39fa22023d0174d2da90604c28c7e0cadafbaa2
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-04 10:37:34 +0900

    Create 20181104.php

---
+++ www/20181104.php
@@ -0,0 +1,13 @@
+<?php
+
+$url = 'http://www.cf.city.hiroshima.jp/saeki-cs/sche6_park/sche6.cgi?year=2018&mon=11'
+
+$res = file_get_contents($url);
+
+$tmp = explode('<col span=1 align=right>', $res);
+$tmp = explode('</table>', $tmp[1]);
+
+$rc = preg_match('/<tr .+?<b>(.+?)<.*>(.*)<\/td><\/tr>/', $tmp[0], $matches);
+
+error_log( print_r($matches, TRUE));
+?>
