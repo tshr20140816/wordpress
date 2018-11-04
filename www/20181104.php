7e5ea6b018c499cf8b10fbdaf2d9c14a89ff6261
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-04 10:42:04 +0900

    Update 20181104.php

--- www/20181104.php
+++ www/20181104.php
@@ -3,11 +3,14 @@
 $url = 'http://www.cf.city.hiroshima.jp/saeki-cs/sche6_park/sche6.cgi?year=2018&mon=11';
 
 $res = file_get_contents($url);
+$res = mb_convert_encoding($res, 'UTF-8', 'SJIS');
+
+error_log($res);
 
 $tmp = explode('<col span=1 align=right>', $res);
 $tmp = explode('</table>', $tmp[1]);
 
 $rc = preg_match('/<tr .+?<b>(.+?)<.*>(.*)<\/td><\/tr>/', $tmp[0], $matches);
 
-error_log( print_r($matches, TRUE));
+error_log(print_r($matches, TRUE));
 ?>
