7ff9b827c2bce349ee09bb6c74db3745cf1d6851
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-05 13:58:10 +0900

    Update 20181105.php

--- www/20181105.php
+++ www/20181105.php
@@ -1,5 +1,31 @@
 <?php
 
+$url = 'https://fukkou-shuyu.jp/';
 
+$res = get_contents($url, [CURLOPT_HEADER => 1]);
+
+error_log($res);
+
+exit();
+
+function get_contents($url_, $options_ = NULL) {
+  $ch = curl_init();
+  curl_setopt_array($ch, [
+    CURLOPT_URL => $url_,
+    CURLOPT_USERAGENT => getenv('USER_AGENT'),
+    CURLOPT_RETURNTRANSFER => TRUE,
+    CURLOPT_ENCODING => '',
+    CURLOPT_FOLLOWLOCATION => 1,
+    CURLOPT_MAXREDIRS => 3,
+    CURLOPT_SSL_FALSESTART => TRUE,
+    ]);
+  if (is_null($options_) == FALSE) {
+    curl_setopt_array($ch, $options_);
+  }
+  $res = curl_exec($ch);
+  curl_close($ch);
+  
+  return $res;
+}
 
 ?>
