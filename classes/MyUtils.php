155068b56838116b26027a78789e2e4a70288490
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-05 15:07:53 +0900

    Update MyUtils.php

--- classes/MyUtils.php
+++ classes/MyUtils.php
@@ -2,7 +2,21 @@
 
 class MyUtils
 {
-  function get_contents($url_, $options_) {
+  private $_pdo;
+  
+  function __construct() {
+    $connection_info = parse_url(getenv('DATABASE_URL'));
+    $pdo = new PDO(
+      "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
+      $connection_info['user'],
+      $connection_info['pass']);
+  }
+  
+  function __destruct() {
+    $_pdo = NULL;
+  }
+  
+  function get_contents($url_, $options_ = NULL) {
     $ch = curl_init();
     curl_setopt_array($ch, [
       CURLOPT_URL => $url_,
