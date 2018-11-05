4517c05b6fe41958b78d1417ca21f33dd8c2850f
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-05 15:15:40 +0900

    Update MyUtils.php

--- classes/MyUtils.php
+++ classes/MyUtils.php
@@ -9,7 +9,7 @@ class MyUtils
     $_pid = getmypid();
     
     $connection_info = parse_url(getenv('DATABASE_URL'));
-    $pdo = new PDO(
+    $_pdo = new PDO(
       "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
       $connection_info['user'],
       $connection_info['pass']);
