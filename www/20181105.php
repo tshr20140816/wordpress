316572435183bc91b8f9b2eb6de316d3919420f6
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-05 09:15:13 +0900

    Update 20181105.php

--- www/20181105.php
+++ www/20181105.php
@@ -14,6 +14,9 @@ for ($i < 0; $i < count($matches); $i++) {
   // error_log(print_r($matches[$i], TRUE));
   $tmp = $matches[$i][1];
   $rc = preg_match('/(.+?)<\/td>.+?<span.*?>(.+?)<\/span>(.*?)<td class="team">(.+?)<\/td>.+?<td class="team">(.+?)<\/td>.*?<td class="">(.+?)<\/td>/s', $tmp, $matches2);
-  error_log(print_r($matches2, TRUE));
+  
+  if ($rc == 1) {
+    error_log(print_r($matches2, TRUE));
+  }
 }
 ?>
