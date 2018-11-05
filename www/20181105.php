2fba3c372504b331e0809690f94a2e146c6d6acf
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-05 09:16:23 +0900

    Update 20181105.php

--- www/20181105.php
+++ www/20181105.php
@@ -13,7 +13,7 @@ $rc = preg_match_all('/<td class="textC">(.+?)<\/tr>/s', $res, $matches, PREG_SE
 for ($i < 0; $i < count($matches); $i++) {
   // error_log(print_r($matches[$i], TRUE));
   $tmp = $matches[$i][1];
-  $rc = preg_match('/(.+?)<\/td>.+?<span.*?>(.+?)<\/span>(.*?)<td class="team">(.+?)<\/td>.+?<td class="team">(.+?)<\/td>.*?<td class="">(.+?)<\/td>/s', $tmp, $matches2);
+  $rc = preg_match('/(.+?)<\/td>.+?<span.*?>(.+?)<\/span>.*?<td class="team">(.+?)<\/td>.+?<td class="team">(.+?)<\/td>.*?<td class="">(.+?)<\/td>/s', $tmp, $matches2);
   
   if ($rc == 1) {
     error_log(print_r($matches2, TRUE));
