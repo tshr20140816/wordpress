04822d24afbe3d45fa05349666d6abf15119032f
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-05 15:14:43 +0900

    Update 20181105.php

--- www/20181105.php
+++ www/20181105.php
@@ -2,7 +2,15 @@
 
 include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+error_log("${pid} START ${requesturi}");
+
 $mu = new MyUtils();
+
+$access_token = $mu->get_access_token();
+
+error_log($pid . ' $access_token : ' . $access_token);
 
 $url = 'https://fukkou-shuyu.jp/';
 
