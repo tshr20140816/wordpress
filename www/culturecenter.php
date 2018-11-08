11c937c5b5d4a9e376e4af5594dae266f5befea1
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-08 15:15:11 +0900

    Update culturecenter.php

--- www/culturecenter.php
+++ www/culturecenter.php
@@ -1,89 +1,27 @@
 <?php
 
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
 $pid = getmypid();
 $requesturi = $_SERVER['REQUEST_URI'];
 error_log("${pid} START ${requesturi}");
 
+$mu = new MyUtils();
+
 // Access Token
+$access_token = $mu->get_access_token();
 
-$connection_info = parse_url(getenv('DATABASE_URL'));
-$pdo = new PDO(
-  "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
-  $connection_info['user'],
-  $connection_info['pass']);
-
-$sql = <<< __HEREDOC__
-SELECT M1.access_token
-      ,M1.refresh_token
-      ,M1.expires_in
-      ,M1.create_time
-      ,M1.update_time
-      ,CASE WHEN LOCALTIMESTAMP < M1.update_time + interval '90 minutes' THEN 0 ELSE 1 END refresh_flag
-  FROM m_authorization M1;
-__HEREDOC__;
-
-$access_token = NULL;
-foreach ($pdo->query($sql) as $row) {
-  $access_token = $row['access_token'];
-  $refresh_token = $row['refresh_token'];
-  $refresh_flag = $row['refresh_flag'];
-}
-
-if ($access_token == NULL) {
-  error_log("${pid} ACCESS TOKEN NONE");
-  exit();
-}
-
-if ($refresh_flag == 1) {
-  error_log("${pid} refresh_token : ${refresh_token}");
-  $post_data = ['grant_type' => 'refresh_token', 'refresh_token' => $refresh_token];
-  
-  $res = get_contents(
-    'https://api.toodledo.com/3/account/token.php',
-    [CURLOPT_USERPWD => getenv('TOODLEDO_CLIENTID') . ':' . getenv('TOODLEDO_SECRET'),
-     CURLOPT_POST => TRUE,
-     CURLOPT_POSTFIELDS => http_build_query($post_data),
-    ]);
-  
-  error_log("${pid} token.php RESPONSE : ${res}");
-  $params = json_decode($res, TRUE);
-
-  $sql = <<< __HEREDOC__
-UPDATE m_authorization
-   SET access_token = :b_access_token
-      ,refresh_token = :b_refresh_token
-      ,update_time = LOCALTIMESTAMP;
-__HEREDOC__;
-
-  $statement = $pdo->prepare($sql);
-  $rc = $statement->execute([':b_access_token' => $params['access_token'],
-                             ':b_refresh_token' => $params['refresh_token']]);
-  error_log("${pid} UPDATE RESULT : ${rc}");
-  
-  $access_token = $params['access_token'];
-}
-
-$pdo = null;
+// Get Contexts
+$list_context_id = $mu->get_contexts();
 
 // Get Folders
-
-$res = get_contents('https://api.toodledo.com/3/folders/get.php?access_token=' . $access_token, NULL);
-$folders = json_decode($res, TRUE);
-
-$private_folder_id = 0;
-for ($i = 0; $i < count($folders); $i++) {
-  if ($folders[$i]['name'] == 'PRIVATE') {
-    $private_folder_id = $folders[$i]['id'];
-    error_log("${pid} PRIVATE FOLDER ID : ${private_folder_id}");
-    break;
-  }
-}
+$private_folder_id = $mu->get_folder_id('PRIVATE');
 
 // Get Tasks
 
 $url = 'https://api.toodledo.com/3/tasks/get.php?comp=0&fields=tag&access_token=' . $access_token
   . '&after=' . strtotime('-2 day');
-$res = get_contents($url, NULL);
+$res = $mu->get_contents($url);
 
 $tasks = json_decode($res, TRUE);
 
@@ -97,37 +35,6 @@ for ($i = 0; $i < count($tasks); $i++) {
 }
 error_log($pid . ' $list_delete_task : ' . print_r($list_delete_task, TRUE));
 
-// Get Contexts
-$res = get_contents('https://api.toodledo.com/3/contexts/get.php?access_token=' . $access_token, NULL);
-$contexts = json_decode($res, TRUE);
-$list_context_id = [];
-for ($i = 0; $i < count($contexts); $i++) {
-  switch ($contexts[$i]['name']) {
-    case '日......':
-      $list_context_id[0] = $contexts[$i]['id'];
-      break;
-    case '.月.....':
-      $list_context_id[1] = $contexts[$i]['id'];
-      break;
-    case '..火....':
-      $list_context_id[2] = $contexts[$i]['id'];
-      break;
-    case '...水...':
-      $list_context_id[3] = $contexts[$i]['id'];
-      break;
-    case '....木..':
-      $list_context_id[4] = $contexts[$i]['id'];
-      break;
-    case '.....金.':
-      $list_context_id[5] = $contexts[$i]['id'];
-      break;
-    case '......土':
-      $list_context_id[6] = $contexts[$i]['id'];
-      break;
-  }
-}
-error_log($pid . ' $list_context_id : ' . print_r($list_context_id, TRUE));
-
 // Culture Center
 
 $y = date('Y');
@@ -138,7 +45,7 @@ for ($j = 0; $j < 2; $j++) {
   $url = 'http://www.cf.city.hiroshima.jp/saeki-cs/sche6_park/sche6.cgi?year=' . $y . '&mon=' . $m;
   error_log($pid . ' $url : ' . $url);
   
-  $res = get_contents($url, NULL);
+  $res = $mu->get_contents($url);
   $res = mb_convert_encoding($res, 'UTF-8', 'SJIS');
 
   // error_log($res);
@@ -186,7 +93,7 @@ $post_data = ['access_token' => $access_token, 'tasks' => '[' . implode(',', $li
 
 // error_log(http_build_query($post_data));
 
-$res = get_contents(
+$res = $mu->get_contents(
   'https://api.toodledo.com/3/tasks/add.php',
   [CURLOPT_POST => TRUE,
    CURLOPT_POSTFIELDS => http_build_query($post_data),
@@ -200,7 +107,7 @@ $tmp = '[{"title":"' . date('Y/m/d H:i:s', strtotime('+ 9 hours')) . ' ' . $requ
   . '","duedate":"' . mktime(0, 0, 0, 1, 1, 2018). '"}]';
 $post_data = ['access_token' => $access_token, 'tasks' => $tmp];
 
-$res = get_contents(
+$res = $mu->get_contents(
   'https://api.toodledo.com/3/tasks/add.php',
   [CURLOPT_POST => TRUE,
    CURLOPT_POSTFIELDS => http_build_query($post_data),
@@ -214,7 +121,7 @@ error_log("${pid} DELETE TARGET TASK COUNT : " . count($list_delete_task));
 
 if (count($list_delete_task) > 0) {
   $post_data = ['access_token' => $access_token, 'tasks' => '[' . implode(',', $list_delete_task) . ']'];  
-  $res = get_contents(
+  $res = $mu->get_contents(
     'https://api.toodledo.com/3/tasks/delete.php',
     [CURLOPT_POST => TRUE,
      CURLOPT_POSTFIELDS => http_build_query($post_data),
@@ -225,24 +132,4 @@ if (count($list_delete_task) > 0) {
 error_log("${pid} FINISH");
 
 exit();
-
-function get_contents($url_, $options_) {
-  $ch = curl_init();
-  curl_setopt_array($ch, [
-    CURLOPT_URL => $url_,
-    CURLOPT_USERAGENT => getenv('USER_AGENT'),
-    CURLOPT_RETURNTRANSFER => TRUE,
-    CURLOPT_ENCODING => '',
-    CURLOPT_FOLLOWLOCATION => 1,
-    CURLOPT_MAXREDIRS => 3,
-    CURLOPT_SSL_FALSESTART => TRUE,
-    ]);
-  if (is_null($options_) == FALSE) {
-    curl_setopt_array($ch, $options_);
-  }
-  $res = curl_exec($ch);
-  curl_close($ch);
-  
-  return $res;
-}
 ?>
