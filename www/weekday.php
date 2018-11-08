c09c69ae008dc452cd274ea952355eaf787d50ce
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-08 11:56:56 +0900

    Update weekday.php

--- www/weekday.php
+++ www/weekday.php
@@ -101,7 +101,9 @@ error_log($pid . ' $list_context_id : ' . print_r($list_context_id, TRUE));
 
 // Get Tasks
 
-$res = get_contents('https://api.toodledo.com/3/tasks/get.php?comp=0&fields=duedate,context&access_token=' . $access_token, NULL);
+$url = 'https://api.toodledo.com/3/tasks/get.php?comp=0&fields=duedate,context&access_token=' . $access_token
+  . '&after=' . strtotime('-2 day');
+$res = get_contents('$url, NULL);
 // error_log($res);
 
 $tasks = json_decode($res, TRUE);
