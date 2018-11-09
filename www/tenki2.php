22b985ae3b8d8e61c820b6e3781632c4068a56a7
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-09 09:12:10 +0900

    Update tenki2.php

--- www/tenki2.php
+++ www/tenki2.php
@@ -186,13 +186,15 @@ if (count($list_weather) == 0) {
 
 // Get Tasks
 
-$url = 'https://api.toodledo.com/3/tasks/get.php?access_token=' . $access_token . '&comp=0&fields=tag,folder,duedate'
+$url = 'https://api.toodledo.com/3/tasks/get.php?comp=0&fields=tag,folder,duedate&access_token=' . $access_token
   . '&after=' . strtotime('-2 day');
 $res = $mu->get_contents($url);
 // error_log($res);
 
 $tasks = json_decode($res, TRUE);
 // error_log($pid . ' $tasks : ' . print_r($tasks, TRUE));
+
+file_put_contents('/tmp/tasks_tenki2', serialize($tasks));
 
 $list_delete_task = [];
 for ($i = 0; $i < count($tasks); $i++) {
