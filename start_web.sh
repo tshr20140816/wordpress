a158fd559d19ef4edee6287768699c8721ce82fb
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-14 11:03:34 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -14,8 +14,21 @@ if [ ! -v BASIC_PASSWORD ]; then
   exit
 fi
 
+current_version=$(cat composer.lock | grep version | awk '{print $2}' | tr -d ,)
+composer update > /dev/null 2>&1 &
+
 export USER_AGENT=$(curl https://raw.githubusercontent.com/tshr20140816/heroku-mode-07/master/useragent.txt)
 
 htpasswd -c -b .htpasswd ${BASIC_USER} ${BASIC_PASSWORD}
+
+wait
+# heroku-buildpack-php
+new_version=$(cat composer.lock | grep version | awk '{print $2}' | tr -d ,)
+
+if [ $current_version = $new_version ]; then
+  echo "heroku-buildpack-php : latest version"
+else
+  echo "heroku-buildpack-php : old version"
+fi
 
 vendor/bin/heroku-php-apache2 -C apache.conf www
