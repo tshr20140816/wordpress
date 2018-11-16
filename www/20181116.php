d99500a3b35362b5361c65a23e2589f14ed3fd61
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-16 17:17:02 +0900

    Update 20181116.php

--- www/20181116.php
+++ www/20181116.php
@@ -8,11 +8,10 @@ $url = 'http://www.jma.go.jp/jp/amedas_h/today-44132.html';
 
 $res = $mu->get_contents($url);
 
-error_log($res);
+// error_log($res);
 
-$tmp = explode("\n", $res);
-for ($i = 0; $i < count($tmp); $i++) {
-  error_log(strip_tags($tmp[$i], '<td>'));
-}
+$tmp = explode('<td class="time left">時</td>', $res);
+$tmp = explode('</table>', $tmp[1]);
 
+error_log($tmp[0]);
 ?>
