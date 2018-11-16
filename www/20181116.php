6221c304c79c2a3b45ee05d826bd3fdb8270f1ee
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-17 06:24:24 +0900

    Update 20181116.php

--- www/20181116.php
+++ www/20181116.php
@@ -15,11 +15,10 @@ $tmp = explode('</table>', $tmp[1]);
 
 // error_log($tmp[0]);
 
-// $rc = preg_match_all('/<tr>(.*?)<td(.*?)>(.+?)<\/td><td(.*?)>(.+?)<\/td><td(.*?)>(.+?)<\/td><td(.*?)>(.+?)<\/td><td(.*?)>(.+?)<\/td><td(.*?)>(.+?)<\/td><td(.*?)>(.+?)<\/td>(.+?)<\/tr>/s', $tmp[0], $matches, PREG_SET_ORDER);
-$rc = preg_match_all('/<tr>(.*?)<td(.*?)>(.+?)<\/td>(.*?)<td(.*?)>(.+?)<\/td><td(.*?)>(.+?)<\/td><td(.*?)>(.+?)<\/td><td(.*?)>(.+?)<\/td><td(.*?)>(.+?)<\/td><td(.*?)><td(.*?)>(.+?)<\/td>(.+?)<\/td>(.+?)<\/tr>/s', $tmp[0], $matches, PREG_SET_ORDER);
+$rc = preg_match_all('/<tr>(.*?)<td(.*?)>(.+?)<\/td>(.*?)<td(.*?)>(.+?)<\/td><td(.*?)>(.+?)<\/td><td(.*?)>(.+?)<\/td><td(.*?)>(.+?)<\/td><td(.*?)>(.+?)<\/td><td(.*?)><td(.*?)>(.+?)<\/td>(.+?)<\/td>(.*?)<\/tr>/s', $tmp[0], $matches, PREG_SET_ORDER);
 
 error_log(print_r($matches, TRUE));
 
-// $hour = 
+//$hour = $matches
 
 ?>
