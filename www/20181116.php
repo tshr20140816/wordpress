dc02d124d55dadf49f0cbce9ae4bdd4dce063327
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-16 16:23:04 +0900

    Update 20181116.php

--- www/20181116.php
+++ www/20181116.php
@@ -4,24 +4,10 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $mu = new MyUtils();
 
-$url = 'https://map.yahooapis.jp/weather/V1/place?interval=5&output=json&appid=' . getenv('YAHOO_API_KEY')
-  . '&coordinates=' . getenv('LONGITUDE') . ',' . getenv('LATITUDE');
+$url = 'http://www.jma.go.jp/jp/amedas_h/today-44132.html';
 
 $res = $mu->get_contents($url);
 
 error_log($res);
 
-$data = json_decode($res, TRUE);
-$data = $data['Feature'][0]['Property']['WeatherList']['Weather'];
-
-error_log(print_r($data, TRUE));
-
-$list = [];
-for ($i = 0; $i < count($data); $i++) {
-  // error_log(print_r($data[$i], TRUE));
-  $list[] = substr($data[$i]['Date'], 8) . ' ' . $data[$i]['Rainfall'];
-}
-error_log(print_r($list, TRUE));
-
-error_log(date('H'));
 ?>
