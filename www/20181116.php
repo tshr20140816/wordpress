00dec6d4dd95878f64180c8f90e734d602ec3f5e
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-16 14:02:02 +0900

    Update 20181116.php

--- www/20181116.php
+++ www/20181116.php
@@ -4,10 +4,14 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $mu = new MyUtils();
 
-$url = 'https://map.yahooapis.jp/weather/V1/place?appid=' . getenv('YAHOO_API_KEY') . '&coordinates=139.732293,35.663613';
+$url = 'https://map.yahooapis.jp/weather/V1/place?interval=5&output=json&appid=' . getenv('YAHOO_API_KEY')
+  . '&coordinates=139.732293,35.663613';
 
 $res = $mu->get_contents($url);
 
 error_log($res);
 
+$data = json_decode($res, TRUE);
+
+error_log($data);
 ?>
