09cf762dc9fa71cae08af6647de58f3e6dde8fa6
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-17 07:16:19 +0900

    Update 20181116.php

--- www/20181116.php
+++ www/20181116.php
@@ -4,7 +4,7 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $mu = new MyUtils();
 
-$url = 'http://www.jma.go.jp/jp/amedas_h/today-44132.html';
+$url = 'http://www.jma.go.jp/jp/amedas_h/today-' . getenv('AMEDAS') . '.html';
 
 $res = $mu->get_contents($url);
 
