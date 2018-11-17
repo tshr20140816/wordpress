a720f3d32032ea95ee896678c99ce969f92d187f
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-17 13:53:34 +0900

    Update tenki2.php

--- www/tenki2.php
+++ www/tenki2.php
@@ -294,8 +294,8 @@ function get_task_soccer($mu_) {
   }
   $count_task = count($list_add_task);
   $list_add_task[] = '{"title":"' . date('Y/m/d H:i:s', strtotime('+ 9 hours')) . '  Soccer Task Add : ' . $count_task
-    . '","context":"' . $list_context_id[1]
-    . '","duedate":"' . mktime(0, 0, 0, 1, 1, 2018) . '"}';
+    . '","context":"' . $list_context_id[mktime(0, 0, 0, 1, 4, 2018)]
+    . '","duedate":"' . mktime(0, 0, 0, 1, 4, 2018) . '"}';
   error_log(getmypid() . ' TASKS SOCCER : ' . print_r($list_add_task, TRUE));
   
   return $list_add_task;
@@ -352,8 +352,8 @@ function get_task_culturecenter($mu_) {
   }
   $count_task = count($list_add_task);
   $list_add_task[] = '{"title":"' . date('Y/m/d H:i:s', strtotime('+ 9 hours')) . '  Culture Center Task Add : ' . $count_task
-    . '","context":"' . $list_context_id[1]
-    . '","duedate":"' . mktime(0, 0, 0, 1, 1, 2018) . '"}';
+    . '","context":"' . $list_context_id[mktime(0, 0, 0, 1, 4, 2018)]
+    . '","duedate":"' . mktime(0, 0, 0, 1, 4, 2018) . '"}';
   error_log(getmypid() . ' TASKS CULTURECENTER : ' . print_r($list_add_task, TRUE));
 
   return $list_add_task;
