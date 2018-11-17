dccb2c7a6a39e9cc9a9f0f727bdfbad796e1948f
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-17 13:01:32 +0900

    Create 20181117.php

---
+++ www/20181117.php
@@ -0,0 +1,15 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+error_log("${pid} START ${requesturi}");
+
+const LIST_YOBI = array('日', '月', '火', '水', '木', '金', '土');
+
+$mu = new MyUtils();
+
+
+
+?>
