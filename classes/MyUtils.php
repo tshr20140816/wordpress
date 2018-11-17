f22161ac8ff2ad393f812e1ee198ce980a472682
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-17 10:21:34 +0900

    Update MyUtils.php

--- classes/MyUtils.php
+++ classes/MyUtils.php
@@ -12,11 +12,11 @@ class MyUtils
       $connection_info['pass']);
   }
   
-  function get_access_token() {
+  function get_access_token($force_refresh_ = FALSE) {
     
     $file_name = '/tmp/access_token';
     
-    if (file_exists($file_name)) {
+    if ($force_refresh_ === FALSE && file_exists($file_name)) {
       $timestamp = filemtime($file_name);
       if ($timestamp > strtotime('-15 minutes')) {
         $access_token = file_get_contents($file_name);
@@ -50,7 +50,7 @@ __HEREDOC__;
       exit();
     }
     
-    if ($refresh_flag == 1) {
+    if ($refresh_flag == 1 || $force_refresh_ === TRUE) {
       error_log(getmypid() . " refresh_token : ${refresh_token}");
       $post_data = ['grant_type' => 'refresh_token', 'refresh_token' => $refresh_token];
 
@@ -84,9 +84,11 @@ __HEREDOC__;
     
     $this->$_access_token = $access_token;
     
-    file_put_contents($file_name, $access_token); // For Cache
+    $this->get_contexts($force_refresh_);
     
-    return $access_token;
+    file_put_contents($file_name, $this->$_access_token); // For Cache
+    
+    return $this->$_access_token;
   }
   
   function get_folder_id($folder_name_) {
@@ -112,10 +114,10 @@ __HEREDOC__;
     return $target_folder_id;
   }
   
-  function get_contexts() {
+  function get_contexts($force_refresh_ = FALSE) {
     
     $file_name = '/tmp/contexts';
-    if (file_exists($file_name)) {
+    if ($force_refresh_ === FALSE && file_exists($file_name)) {
       $list_context_id = unserialize(file_get_contents($file_name));
       error_log(getmypid() . ' (CACHE HIT) $list_context_id : ' . print_r($list_context_id, TRUE));
       return $list_context_id;
@@ -259,11 +261,20 @@ __HEREDOC__;
       curl_close($ch);
       if ($http_code == '200') {
         break;
+      }
+      
+      error_log(getmypid() . ' $res : ' . $res);
+      
+      if ($http_code == '401') {
+        if ($res == '{"errorCode":2,"errorDesc":"Unauthorized","errors":[{"status":"2","message":"Unauthorized"}]}') {
+          $this->get_access_token(TRUE);
+          error_log(getmypid() . ' RETRY URL : ' . $url_);
+        } else {
+          break;
+        }
       } else if ($http_code != '503') {
-        error_log(getmypid() . ' $res : ' . $res);
         break;
       } else {
-        error_log(getmypid() . ' $res : ' . $res);
         sleep(3);
         error_log(getmypid() . ' RETRY URL : ' . $url_);
       }
