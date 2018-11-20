7087867fe7a2480af254ccefc4d7add86fda0df8
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-20 11:39:21 +0900

    Create 20181120.php

---
+++ www/20181120.php
@@ -0,0 +1,18 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$yyyy = '2018';
+$mm = '12';
+
+$res = $mu->get_contents('https://eco.mtk.nao.ac.jp/koyomi/dni/' . $yyyy . '/s' . getenv('AREA_ID') . $mm . '.html');
+
+$tmp = explode('<table ', $res);
+$tmp = explode('</table>', $tmp[1]);
+$tmp = explode('</tr>', $tmp[0]);
+array_shift($tmp);
+array_pop($tmp);
+
+error_log(print_r($tmp, TRUE);
+
+?>
