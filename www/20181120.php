e0ed378764373341f9a9464136afb690218e6ffe
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-20 14:43:21 +0900

    Update 20181120.php

--- www/20181120.php
+++ www/20181120.php
@@ -4,17 +4,14 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $mu = new MyUtils();
 
-$yyyy = '2018';
-$mm = '12';
+$url = 'https://www.w-nexco.co.jp/traffic_info/construction/traffic.php?fdate='
+  . date('Ymd', strtotime('+1 day'))
+  . '&tdate='
+  . date('Ymd', strtotime('+14 day'))
+  . '&ak=1&ac=1&kisei%5B%5D=901&dirc%5B%5D=1&dirc%5B%5D=2&order=1&ronarrow=1&road%5B%5D=1011&road%5B%5D=1912&road%5B%5D=1020&road%5B%5D=225A&road%5B%5D=1201&road%5B%5D=1222&road%5B%5D=1231&road%5B%5D=234D&road%5B%5D=1232';
 
-$res = $mu->get_contents('https://eco.mtk.nao.ac.jp/koyomi/dni/' . $yyyy . '/s' . getenv('AREA_ID') . $mm . '.html');
+$res = $mu->get_contents($url);
 
-$tmp = explode('<table ', $res);
-$tmp = explode('</table>', $tmp[1]);
-$tmp = explode('</tr>', $tmp[0]);
-array_shift($tmp);
-array_pop($tmp);
-
-error_log(print_r($tmp, TRUE));
+error_log($res);
 
 ?>
