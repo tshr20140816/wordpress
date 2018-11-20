a49744ee9f620a01464dccd27fb30a8bfe2144d9
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-20 14:57:31 +0900

    Update 20181120.php

--- www/20181120.php
+++ www/20181120.php
@@ -8,15 +8,15 @@ $url = 'https://www.w-nexco.co.jp/traffic_info/construction/traffic.php?fdate='
   . date('Ymd', strtotime('+1 day'))
   . '&tdate='
   . date('Ymd', strtotime('+14 day'))
-  . '&ak=1&ac=1&kisei%5B%5D=901&dirc%5B%5D=1&dirc%5B%5D=2&order=1&ronarrow=1&road%5B%5D=1011&road%5B%5D=1912&road%5B%5D=1020&road%5B%5D=225A&road%5B%5D=1201&road%5B%5D=1222&road%5B%5D=1231&road%5B%5D=234D&road%5B%5D=1232';
+  . '&ak=1&ac=1&kisei%5B%5D=901&dirc%5B%5D=1&dirc%5B%5D=2&order=2&ronarrow=1&road%5B%5D=1011&road%5B%5D=1912&road%5B%5D=1020&road%5B%5D=225A&road%5B%5D=1201&road%5B%5D=1222&road%5B%5D=1231&road%5B%5D=234D&road%5B%5D=1232&road%5B%5D=1260';
 
 $res = $mu->get_contents($url);
 
-//<h3 class="accent lb10">
+//<table cellspacing="0" summary="" class="lb05">
 
-$tmp = explode('h3 class="accent lb10">', $res);
+$tmp = explode('<!--工事日程順-->', $res);
+$tmp = explode('<table cellspacing="0" summary="" class="lb05">', $tmp[0]);
+$tmp = $tmp[1];
 
-array_shift($tmp);
-
-error_log(print_r($tmp, TRUE));
+error_log($tmp);
 ?>
