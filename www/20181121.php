8d082ca8f96f1b6e6e573da94a1145ff4b72668d
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-21 12:36:45 +0900

    Update 20181121.php

--- www/20181121.php
+++ www/20181121.php
@@ -4,11 +4,11 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $mu = new MyUtils();
 
-$post_data = ['year' => 2018,
-              'month' => 11,
-              'day' => 30,
+$post_data = ['year' => '2018',
+              'month' => '11',
+              'day' => '30',
               'id' => getenv('AREA_ID'),
-              'twil' => 0];
+              'twil' => '0'];
 
 $res = $mu->get_contents(
   'https://eco.mtk.nao.ac.jp/cgi-bin/koyomi/sunmoon.cgi',
@@ -19,5 +19,9 @@ $res = $mu->get_contents(
 $res = mb_convert_encoding($res, 'UTF-8', 'EUC-JP');
 
 error_log($res);
+//<table class="koyomi" summary="Table">
+
+$tmp = explode('<table class="koyomi" summary="Table">', $res);
+//error_log($tmp[1]);
 
 ?>
