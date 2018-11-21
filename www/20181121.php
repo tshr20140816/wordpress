a99873ceb8533b8435383cfdde51d21de5c19ecd
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-21 11:58:52 +0900

    Update 20181121.php

--- www/20181121.php
+++ www/20181121.php
@@ -4,5 +4,10 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $mu = new MyUtils();
 
+$yyyy = date('Y');
+$mm = date('m');
+$res = $mu->get_contents('https://eco.mtk.nao.ac.jp/koyomi/dni/' . $yyyy . '/s' . getenv('AREA_ID') . $mm . '.html');
+
+
 
 ?>
