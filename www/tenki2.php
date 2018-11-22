f538cafb23314c812e75a7796913f63b7336107f
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-22 11:27:53 +0900

    Update tenki2.php

--- www/tenki2.php
+++ www/tenki2.php
@@ -445,13 +445,13 @@ function get_task_sun($mu_) {
     if ($rc == 1) {
       $rc = preg_match('/.+?<\/td>.*?<td>(.+?)<\/td>.*?<td>.+?<\/td>.*?<td>.+?<\/td>.*?<td>.+?<\/td>.*?<td>(.+?)</', $tmp[$i], $matches);
       
-      $tmp = date('m/d', $timestamp) . ' 0' . trim($matches[1] . ' 日の出');
+      $tmp = date('m/d', $timestamp) . ' 0' . trim($matches[1]) . ' 日の出';
       $tmp = str_replace('__TITLE__', $tmp, $add_task_template);
       $tmp = str_replace('__DUEDATE__', $timestamp, $tmp);
       $tmp = str_replace('__CONTEXT__', $list_context_id[date('w', $timestamp)], $tmp);
       $list_add_task[] = $tmp;
       
-      $tmp = date('m/d', $timestamp) . ' ' . trim($matches[2] . ' 日の入り');
+      $tmp = date('m/d', $timestamp) . ' ' . trim($matches[2]) . ' 日の入り';
       $tmp = str_replace('__TITLE__', $tmp, $add_task_template);
       $tmp = str_replace('__DUEDATE__', $timestamp, $tmp);
       $tmp = str_replace('__CONTEXT__', $list_context_id[date('w', $timestamp)], $tmp);
