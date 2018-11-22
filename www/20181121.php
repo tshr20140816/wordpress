20536c80acf3f12effc69b3b3a9bcf28b094fed8
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-22 09:11:25 +0900

    Update 20181121.php

--- www/20181121.php
+++ www/20181121.php
@@ -4,25 +4,19 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $mu = new MyUtils();
 
-$post_data = ['year' => '2018',
-              'month' => '11',
-              'day' => '30',
-              'id' => getenv('AREA_ID'),
-              'tom' => '',
-              'twil' => '0'];
+$timestamp = strtotime('+1 day');
+$yyyy = date('Y', $timestamp);
+$mm = date('m', $timestamp);
 
-$res = $mu->get_contents(
-  'https://eco.mtk.nao.ac.jp/cgi-bin/koyomi/sunmoon.cgi',
-  [CURLOPT_POST => TRUE,
-   CURLOPT_POSTFIELDS => http_build_query($post_data),
-  ]);
+$res = $mu->get_contents('https://eco.mtk.nao.ac.jp/koyomi/dni/' . $yyyy . '/s' . getenv('AREA_ID') . $mm . '.html');
 
 $res = mb_convert_encoding($res, 'UTF-8', 'EUC-JP');
 
-error_log($res);
-//<table class="koyomi" summary="Table">
+$tmp = explode('<table ', $res);
+$tmp = explode('</table>', $tmp[1]);
+$tmp = explode('</tr>', $tmp[0]);
+array_shift($tmp);
+array_pop($tmp);
 
-$tmp = explode('<table class="koyomi" summary="Table">', $res);
-//error_log($tmp[1]);
-
+error_log(print_r($tmp, TRUE));
 ?>
