b5c56c43f783722ca698c1e7858881f3d4e79d37
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-26 11:49:35 +0900

    Update 20181126.php

--- www/20181126.php
+++ www/20181126.php
@@ -16,6 +16,20 @@ error_log(print_r($headers, TRUE));
 
 for ($i = 0; $i < count($headers); $i++) {
   error_log(trim(strip_tags($headers[$i])));
+  switch (trim(strip_tags($headers[$i]))) {
+    case '気温':
+      $index_temp = $i + 1;
+    case '降水量':
+      $index_rain = $i + 1;
+    case '風向':
+      $index_wind = $i + 1;
+    case '風速':
+      $index_wind_speed = $i + 1;
+    case '湿度':
+      $index_humi = $i + 1;
+    case '気圧':
+      $index_pres = $i + 1;
+  }
 }
 
 $rc = preg_match_all('/<tr>.*?<td.*?>(.+?)<\/td>.*?' . str_repeat('<td.*?>(.+?)<\/td>', count($headers) - 1) . '.+?<\/tr>/s'
@@ -24,5 +38,20 @@ $rc = preg_match_all('/<tr>.*?<td.*?>(.+?)<\/td>.*?' . str_repeat('<td.*?>(.+?)<
 array_shift($matches);
 
 error_log(print_r($matches, TRUE));
+
+$title
+for ($i = 0; $i < count($matches); $i++) {
+  $hour = $matches[$i][0];
+  $temp = $matches[$i][$index_temp];
+  $rain = $matches[$i][$index_rain];
+  $wind = $matches[$i][$index_wind] . $matches[$i][$index_wind_speed];
+  $humi = $matches[$i][$index_humi];
+  $pres = $matches[$i][$index_pres];
+  if ($temp == '&nbsp;') {
+    continue;
+  }
+  error_log("${pid} ${hour}時 ${temp}℃ ${humi}% ${rain}mm ${wind}m/s ${pres}hPa");
+  $title = "${hour}時 ${temp}℃ ${humi}% ${rain}mm ${wind}m/s ${pres}hPa";
+}
 
 ?>
