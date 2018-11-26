9f9b92cae76cd6632833ec719a4469dba20eaeff
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-26 11:22:43 +0900

    Update 20181126.php

--- www/20181126.php
+++ www/20181126.php
@@ -18,8 +18,12 @@ error_log($tmp1[0]);
 //$rc = mb_substr_count($tmp1[0], '</td>');
 //error_log($rc);
 
-$tmp1 = explode('</td>', $tmp1[0]);
-error_log(print_r($tmp1, TRUE));
+$headers = explode('</td>', $tmp1[0]);
+error_log(print_r($headers, TRUE));
+
+for ($i = 0; $i < count($headers); $i++) {
+  error_log(strip_tags($headers[$i]));
+}
 
 /*
 $rc = preg_match_all('/<tr>(.*?)<td(.*?)>(.+?)<\/td>(.*?)' . str_repeat('<td(.*?)>(.+?)<\/td>', 8) . '(.+?)<\/tr>/s'
