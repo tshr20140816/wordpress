4ee18d98bf8e640c32f9a0ba2ec094aa30d2729c
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-26 09:45:25 +0900

    Update 20181126.php

--- www/20181126.php
+++ www/20181126.php
@@ -6,11 +6,12 @@ $mu = new MyUtils();
 
 $res = $mu->get_contents('http://www.jma.go.jp/jp/amedas_h/today-' . getenv('AMEDAS') . '.html');
 
-$tmp = explode('<td class="time left">時</td>', $res);
+$tmp = explode('">時刻</td>', $res);
 $tmp = explode('</table>', $tmp[1]);
 
 $rc = preg_match_all('/<tr>(.*?)<td(.*?)>(.+?)<\/td>(.*?)' . str_repeat('<td(.*?)>(.+?)<\/td>', 8) . '(.+?)<\/tr>/s'
                      , $tmp[0], $matches, PREG_SET_ORDER);
                      
+error_log(print_r($matches, TRUE));
 
 ?>
