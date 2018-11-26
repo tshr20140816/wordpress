f47b2a2e5f6e9aaae6219666e919da5660eb5d13
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-26 09:26:42 +0900

    Update hourly.php

--- www/hourly.php
+++ www/hourly.php
@@ -29,7 +29,7 @@ $res = $mu->get_contents('http://www.jma.go.jp/jp/amedas_h/today-' . getenv('AME
 $tmp = explode('<td class="time left">時</td>', $res);
 $tmp = explode('</table>', $tmp[1]);
 
-$rc = preg_match_all('/<tr>(.*?)<td(.*?)>(.+?)<\/td>(.*?)' . str_repeat('<td(.*?)>(.+?)<\/td>', 7) . '(.+?)<\/tr>/s'
+$rc = preg_match_all('/<tr>(.*?)<td(.*?)>(.+?)<\/td>(.*?)' . str_repeat('<td(.*?)>(.+?)<\/td>', 8) . '(.+?)<\/tr>/s'
                      , $tmp[0], $matches, PREG_SET_ORDER);
 
 $title = '';
