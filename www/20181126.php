eac15a9804610de2d3bf27992e98ad74d9d4e111
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-26 09:52:13 +0900

    Update 20181126.php

--- www/20181126.php
+++ www/20181126.php
@@ -9,11 +9,16 @@ $res = $mu->get_contents('http://www.jma.go.jp/jp/amedas_h/today-' . getenv('AME
 $tmp = explode('">時刻</td>', $res);
 $tmp = explode('</table>', $tmp[1]);
 
-error_log($tmp[0]);
+//error_log($tmp[0]);
 
+$tmp1 = explode('</tr>', $tmp[0]);
+
+error_log($tmp1[0]);
+
+/*
 $rc = preg_match_all('/<tr>(.*?)<td(.*?)>(.+?)<\/td>(.*?)' . str_repeat('<td(.*?)>(.+?)<\/td>', 8) . '(.+?)<\/tr>/s'
                      , $tmp[0], $matches, PREG_SET_ORDER);
                      
 error_log(print_r($matches, TRUE));
-
+*/
 ?>
