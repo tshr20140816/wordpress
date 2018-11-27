b6d6ae2620e5e02783f523f4925a81ad4ca068bc
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-11-27 11:36:28 +0900

    Update 20181127.php

--- www/20181127.php
+++ www/20181127.php
@@ -5,7 +5,7 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 $mu = new MyUtils();
 
 $url = 'https://map.yahooapis.jp/search/local/V1/localSearch?output=json&appid=' . getenv('YAHOO_API_KEY')
-  . '&results=100&detail=full&dist=20&lon=' . getenv('LONGITUDE') . '&lat=' . getenv('LATITUDE');
+  . '&results=100&detail=simple&dist=20&lon=' . getenv('LONGITUDE') . '&lat=' . getenv('LATITUDE');
 $res = $mu->get_contents($url);
 $data = json_decode($res, TRUE);
 error_log(getmypid() . ' $data : ' . print_r($data, TRUE));
