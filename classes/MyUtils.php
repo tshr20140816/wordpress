1ce5f22e51a0ab13addaaf70e90d65774147a349
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-04 15:27:19 +0900

    Update MyUtils.php

--- classes/MyUtils.php
+++ classes/MyUtils.php
@@ -275,19 +275,19 @@ __HEREDOC__;
     }
 
     $sql = <<< __HEREDOC__
-SELECT T1.url
-      ,T1.content
+SELECT T1.url_base64
+      ,T1.content_compress_base64
       ,T1.update_time
       ,CASE WHEN LOCALTIMESTAMP < T1.update_time + interval '1 days' THEN 0 ELSE 1 END refresh_flag
   FROM t_webcache T1
- WHERE T1,url = :b_url
+ WHERE T1.url_base64 = :b_url_base64
 __HEREDOC__;
     
     $pdo = $this->get_pdo();
     
     $statement = $pdo->prepare($sql);
     
-    $statement->execute([':b_url' => $url_]);
+    $statement->execute([':b_url_base64' => $url_]);
     $result = $statement->fetch();
     
     error_log($result);
