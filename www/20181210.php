4d254c266db2bc46b82b7f4785fdb3d774f9c20b
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-10 11:52:51 +0900

    Update 20181210.php

--- www/20181210.php
+++ www/20181210.php
@@ -4,27 +4,12 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $mu = new MyUtils();
 
-$post_data = ['from_year' => 2020];
-// error_log(http_build_query($post_data));
-
-// exit();
+$post_data = ['from_year' => 2019];
 
 $res = $mu->get_contents(
   'http://www.calc-site.com/calendars/solar_year',
   [CURLOPT_POST => TRUE,
    CURLOPT_POSTFIELDS => http_build_query($post_data),
-  ]);
-
-//error_log($res);
-
-exit();
-
-$res = $mu->get_contents(
-  'http://www.calc-site.com/calendars/solar_year?from_year=2020',
-  [CURLOPT_POST => TRUE,
-   CURLOPT_POSTFIELDS => http_build_query($post_data),
-  ]);
-
-error_log($res);
+  ], TRUE);
 
 ?>
