5f334ca3a6779a7b047117587c2873e70e5cb44a
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-10 11:40:02 +0900

    Update 20181210.php

--- www/20181210.php
+++ www/20181210.php
@@ -5,6 +5,10 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 $mu = new MyUtils();
 
 $post_data = ['from_year' => 2020];
+error_log(http_build_query($post_data));
+
+exit();
+
 $res = $mu->get_contents(
   'http://www.calc-site.com/calendars/solar_year',
   [CURLOPT_POST => TRUE,
