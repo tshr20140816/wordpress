938786758a338bd114e0941b75b4b4e73a999392
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-10 09:52:04 +0900

    Update 20181210.php

--- www/20181210.php
+++ www/20181210.php
@@ -1,5 +1,7 @@
 <?php
 
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
 $mu = new MyUtils();
 
 $res = $mu->get_contents('https://eco.mtk.nao.ac.jp/koyomi/dni/2018/s3512.html', NULL, TRUE);
