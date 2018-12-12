b92e6303a5e643d6670dce592d412ab2f18f68ce
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-12 15:11:15 +0900

    Update hourly.php

--- www/hourly.php
+++ www/hourly.php
@@ -4,7 +4,6 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $pid = getmypid();
 $requesturi = $_SERVER['REQUEST_URI'];
-error_log("${pid} " . __METHOD__);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 const LIST_YOBI = array('日', '月', '火', '水', '木', '金', '土');
