c0febac0d7a3f356210a63a61a5f1336710048b0
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-13 08:58:43 +0900

    Update 20181213.php

--- www/20181213.php
+++ www/20181213.php
@@ -8,16 +8,16 @@ $url = 'https://typhoon.yahoo.co.jp/weather/jp/warn/32/32201/';
 
 $res = $mu->get_contents($url);
 
-error_log($res);
-
 $rc = preg_match_all('/<ul class="warnDetail_head_labels">(.+?)<\/ul>/s', $res, $matches, PREG_SET_ORDER);
 
 error_log(print_r($matches, TRUE));
-/*
+
 $url = 'https://typhoon.yahoo.co.jp/weather/jp/warn/5/5201/';
 
 $res = $mu->get_contents($url);
 
-error_log($res);
-*/
+$rc = preg_match_all('/<ul class="warnDetail_head_labels">(.+?)<\/ul>/s', $res, $matches, PREG_SET_ORDER);
+
+error_log(print_r($matches, TRUE));
+
 ?>
