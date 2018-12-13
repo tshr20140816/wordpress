88c992df32a9ef1bdd62be1774d65cdd4759b83f
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-13 09:15:02 +0900

    Update 20181213.php

--- www/20181213.php
+++ www/20181213.php
@@ -15,10 +15,10 @@ error_log(print_r($matches, TRUE));
 $res = preg_replace('/<.+?>/s', ' ', $matches[0][1]);
 error_log($res);
 
-$res = preg_replace('/\s+/s', ' ', $res);
+$res = trim(preg_replace('/\s+/s', ' ', $res));
 error_log($res);
 
-/*
+
 $url = 'https://typhoon.yahoo.co.jp/weather/jp/warn/5/5201/';
 
 $res = $mu->get_contents($url);
@@ -26,5 +26,10 @@ $res = $mu->get_contents($url);
 $rc = preg_match_all('/<ul class="warnDetail_head_labels">(.+?)<\/ul>/s', $res, $matches, PREG_SET_ORDER);
 
 error_log(print_r($matches, TRUE));
-*/
+
+$res = preg_replace('/<.+?>/s', ' ', $matches[0][1]);
+error_log($res);
+
+$res = trim(preg_replace('/\s+/s', ' ', $res));
+error_log($res);
 ?>
