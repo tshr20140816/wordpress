459528bc49c7ded9709e2bde197ac2f12e850552
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-14 14:11:52 +0900

    Update 20181214.php

--- www/20181214.php
+++ www/20181214.php
@@ -9,4 +9,10 @@ $url = 'http://the-outlets-hiroshima.com/static/detail/car';
 $res = $mu->get_contents($url);
 
 error_log($res);
+
+//<p id="parkingnow">
+
+$rc = preg_match('/<p id="parkingnow"><img src="(.+?)"/s', $res, $matches);
+
+error_log(print_r($matches));
 ?>
