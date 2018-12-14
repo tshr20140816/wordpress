51fa1468fe7f767df90e104eaed44bdea9314522
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-14 14:08:57 +0900

    Update 20181214.php

--- www/20181214.php
+++ www/20181214.php
@@ -1,5 +1,12 @@
 <?php
 
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
+$mu = new MyUtils();
 
+$url = 'http://the-outlets-hiroshima.com/static/detail/car';
+
+$res = $mu->get_contents($url);
+
+error_log($res);
 ?>
