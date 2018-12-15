643e03d0cd86547de43ad4260de607d51f01b68b
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-15 23:28:38 +0900

    Update 20181214.php

--- www/20181214.php
+++ www/20181214.php
@@ -10,9 +10,34 @@ $res = $mu->get_contents($url);
 
 error_log($res);
 
-//<p id="parkingnow">
-
 $rc = preg_match('/<p id="parkingnow"><img src="(.+?)"/s', $res, $matches);
 
 error_log(print_r($matches, TRUE));
+
+$filePath = '/tmp/sample_image.jpg';
+file_put_contents($filePath, file_get_contents($matches[1]));
+
+$url = 'https://www.ocrwebservice.com/restservices/processDocument?language=japanese&outputformat=txt&gettext=true&getwords=true';
+
+$session = curl_init();
+
+curl_setopt($session, CURLOPT_USERPWD, "$username:$license_code");
+
+curl_setopt($session, CURLOPT_UPLOAD, true);
+curl_setopt($session, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($session, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($session, CURLOPT_TIMEOUT, 200);
+curl_setopt($session, CURLOPT_HEADER, false);
+curl_setopt($session, CURLOPT_SSL_VERIFYPEER, true);
+curl_setopt($session, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+$fp = fopen($filePath, 'r');
+curl_setopt($session, CURLOPT_INFILESIZE, filesize($filePath));
+$result = curl_exec($session);
+curl_close($session);
+fclose($fp);
+
+$data = json_decode($result);
+
+error_log(print_r($data, TRUE));
 ?>
