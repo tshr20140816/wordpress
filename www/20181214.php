c078d91d54faea5d8e1b8a6e2c1d832f507cccda
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-15 23:59:12 +0900

    Update 20181214.php

--- www/20181214.php
+++ www/20181214.php
@@ -21,17 +21,16 @@ file_put_contents($filePath, $res);
 $res = file_get_contents($filePath);
 error_log(strlen($res));
 
-$url = 'http://www.ocrwebservice.com/restservices/processDocument?language=japanese&outputformat=txt&gettext=true&getwords=true';
-$url = 'http://www.ocrwebservice.com/restservices/processDocument?gettext=true';
-
-$fp = fopen($filePath, 'r');
-$session = curl_init();
-curl_setopt($session, CURLOPT_URL, $url);
+$url = 'http://www.ocrwebservice.com/restservices/processDocument?language=japanese&gettext=true';
+// $url = 'http://www.ocrwebservice.com/restservices/processDocument?gettext=true';
 
 $username = getenv('OCRWEBSERVICE_USER');
 $license_code = getenv('OCRWEBSERVICE_LICENSE_CODE');
-error_log($username);
-error_log($license_code);
+  
+$fp = fopen($filePath, 'r');
+$session = curl_init();
+
+curl_setopt($session, CURLOPT_URL, $url);
 curl_setopt($session, CURLOPT_USERPWD, "$username:$license_code");
 
 curl_setopt($session, CURLOPT_UPLOAD, true);
@@ -39,14 +38,14 @@ curl_setopt($session, CURLOPT_CUSTOMREQUEST, 'POST');
 curl_setopt($session, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($session, CURLOPT_TIMEOUT, 200);
 curl_setopt($session, CURLOPT_HEADER, false);
-//curl_setopt($session, CURLOPT_SSL_VERIFYPEER, true);
+
 curl_setopt($session, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-
-
+ 
+curl_setopt($session, CURLOPT_INFILE, $fp);
 curl_setopt($session, CURLOPT_INFILESIZE, filesize($filePath));
-error_log('C010');
+
 $result = curl_exec($session);
-error_log('C020');
+
 $httpCode = curl_getinfo($session, CURLINFO_HTTP_CODE);
 curl_close($session);
 fclose($fp);
@@ -54,6 +53,5 @@ fclose($fp);
 error_log($httpCode);
 
 $data = json_decode($result);
-
 error_log(print_r($data, TRUE));
 ?>
