224f89a57f6724d932a4979e436c93e185b9d5b3
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-16 22:38:31 +0900

    Create 20181216.php

---
+++ www/20181216.php
@@ -0,0 +1,14 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$mu = new MyUtils();
+
+$url = 'http://the-outlets-hiroshima.com/static/detail/car';
+$res = $mu->get_contents($url);
+
+$rc = preg_match('/<p id="parkingnow"><img src="(.+?)"/s', $res, $matches);
+$res = $mu->get_contents($matches[1]);
+
+
+?>
