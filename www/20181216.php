d300af9dfe3d37685bfe21e2a714120e4f58705f
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-16 21:09:49 +0900

    Update 20181216.php

--- www/20181216.php
+++ www/20181216.php
@@ -5,28 +5,24 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 $mu = new MyUtils();
 
 $url = 'http://the-outlets-hiroshima.com/static/detail/car';
-
 $res = $mu->get_contents($url);
 
 $rc = preg_match('/<p id="parkingnow"><img src="(.+?)"/s', $res, $matches);
-
-error_log(print_r($matches, TRUE));
-
 $res = $mu->get_contents($matches[1]);
 
 $filePath = '/tmp/sample_image.jpg';
 file_put_contents($filePath, $res);
 
-$url = 'http://www.ocrwebservice.com/restservices/processDocument?language=english&gettext=true&getwords=true&newline=1';
 // $url = 'http://www.ocrwebservice.com/restservices/processDocument?gettext=true';
+$url = 'http://www.ocrwebservice.com/restservices/processDocument?language=english&gettext=true&getwords=true&newline=1';
 
-$username = getenv('OCRWEBSERVICE_USER');
-$license_code = getenv('OCRWEBSERVICE_LICENSE_CODE');
+// $username = getenv('OCRWEBSERVICE_USER');
+// $license_code = getenv('OCRWEBSERVICE_LICENSE_CODE');
   
 $fp = fopen($filePath, 'r');
 
 $options = [
-  CURLOPT_USERPWD => "$username:$license_code",
+  CURLOPT_USERPWD => getenv('OCRWEBSERVICE_USER') . ':' . getenv('OCRWEBSERVICE_LICENSE_CODE'),
   CURLOPT_UPLOAD => TRUE,
   CURLOPT_CUSTOMREQUEST => 'POST',
   CURLOPT_TIMEOUT => 200,
