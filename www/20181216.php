23a5b2042b7c241df3201626da1a2ea08e87e9ee
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-16 23:29:56 +0900

    Update 20181216.php

--- www/20181216.php
+++ www/20181216.php
@@ -10,9 +10,29 @@ $res = $mu->get_contents($url);
 $rc = preg_match('/<p id="parkingnow"><img src="(.+?)"/s', $res, $matches);
 // $res = $mu->get_contents($matches[1]);
 
+/*
 $url = 'https://api.ocr.space/parse/imageurl?language=jpn&apikey=' . getenv('OCRSPACE_APIKEY') . '&url=' . $matches[1];
 
 $res = $mu->get_contents($url);
+
+$data = json_decode($res);
+error_log(print_r($data, TRUE));
+*/
+
+$res = $mu->get_contents($matches[1]);
+
+$json = ['requests' => [['image' => ['content' => base64_encode($res)],
+                         'features' => [['type' => 'TEXT_DETECTION',
+                                         'maxResults' => 10]]]]];
+
+$url = 'https://vision.googleapis.com/v1/images:annotate?key=' . getenv('GOOGLE_API_KEY');
+$options = [
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_HTTPHEADER => ['Content-Type: application/json'],
+  CURLOPT_POSTFIELDS => json_encode($json),
+  CURLOPT_REFERER => 'https://' . getenv('HEROKU_APP_NAME') . '.herokuapp.com/',
+  ];
+$res = $mu->get_contents($url, $options);
 
 $data = json_decode($res);
 error_log(print_r($data, TRUE));
