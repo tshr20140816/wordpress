b0bda973d8b712c1f404a0dcb256db7f69310cf7
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-16 22:54:18 +0900

    Update 20181216.php

--- www/20181216.php
+++ www/20181216.php
@@ -10,8 +10,8 @@ $res = $mu->get_contents($url);
 $rc = preg_match('/<p id="parkingnow"><img src="(.+?)"/s', $res, $matches);
 // $res = $mu->get_contents($matches[1]);
 
-$url = 'https://api.ocr.space/parse/imageurl?apikey=' . getenv('OCRSPACE_APIKEY') . '&url=' . $matches[1];
 $url = 'https://api.ocr.space/parse/imageurl?language=jpn&apikey=' . getenv('OCRSPACE_APIKEY') . '&url=' . $matches[1];
+$url = 'https://api.ocr.space/parse/imageurl?apikey=' . getenv('OCRSPACE_APIKEY') . '&url=' . $matches[1];
 
 $res = $mu->get_contents($url);
 
