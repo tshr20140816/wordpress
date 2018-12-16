259e30c1c5bd8997dc0649a8cc5d026c55b42acb
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-16 22:44:45 +0900

    Update 20181216.php

--- www/20181216.php
+++ www/20181216.php
@@ -8,11 +8,12 @@ $url = 'http://the-outlets-hiroshima.com/static/detail/car';
 $res = $mu->get_contents($url);
 
 $rc = preg_match('/<p id="parkingnow"><img src="(.+?)"/s', $res, $matches);
-$res = $mu->get_contents($matches[1]);
+// $res = $mu->get_contents($matches[1]);
 
-$url = 'https://api.ocr.space/parse/imageurl?apikey=' . getenv('OCRSPACE_APIKEY') . '&url=http://i.imgur.com/fwxooMv.png';
+$url = 'https://api.ocr.space/parse/imageurl?language=jpn&apikey=' . getenv('OCRSPACE_APIKEY') . '&url=' . $matches[1];
 
 $res = $mu->get_contents($url);
 
-error_log($res);
+$data = json_decode($res);
+error_log(print_r($data, TRUE));
 ?>
