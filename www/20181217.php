75d9abb6cb0c768da820d02838dadc4734aa64e0
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-17 10:17:39 +0900

    Update 20181217.php

--- www/20181217.php
+++ www/20181217.php
@@ -1,5 +1,18 @@
 <?php
 
-$image = new Imagick();
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$mu = new MyUtils();
+
+$url = 'http://the-outlets-hiroshima.com/static/detail/car';
+$res = $mu->get_contents($url);
+
+$rc = preg_match('/<p id="parkingnow"><img src="(.+?)"/s', $res, $matches);
+$res = $mu->get_contents($matches[1]);
+
+$file = '/tmp/sample.jpg';
+file_put_contents($file, $res);
+
+$im = imagecreatefrompng($file);
 
 ?>
