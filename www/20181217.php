c70d9d43a46c3d73e09e61ce71fa631c74271871
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-17 14:26:05 +0900

    Update 20181217.php

--- www/20181217.php
+++ www/20181217.php
@@ -7,10 +7,7 @@ $mu = new MyUtils();
 $url = 'https://weather.yahoo.co.jp/weather/jp/27/6200.html';
 $res = $mu->get_contents($url);
 
-// <!--指数情報-->
-// <!--/指数情報-->
-
-$rc = preg_match_all('/<!--指数情報-->(.+?)<!--\/指数情報-->/s', $res, $matches, PREG_SET_ORDER);
+$rc = preg_match_all('/<!--指数情報-->.+?<span>傘指数(.+?)<.+?<p class="index_text">(.+?)</s', $res, $matches, PREG_SET_ORDER);
 
 error_log(print_r($matches, TRUE));
 
