d348f88be206c777b1be7e0d31390bcea11a7480
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-17 15:44:45 +0900

    Update 20181217.php

--- www/20181217.php
+++ www/20181217.php
@@ -4,5 +4,25 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $mu = new MyUtils();
 
+$file = '/tmp/parse.txt';
+@unlink($file);
+
+$url = 'https://' . get_env('HEROKU_APP_NAME') . '.herokuapp.com/20181216.php';
+
+$options = [
+  CURLOPT_TIMEOUT => 3,
+  ];
+
+$res = $mu->get_contents($url);
+
+for ($i = 0; $i < 25; $i++) {
+  if (file_exists($file) === TRUE) {
+    break;
+  }
+  error_log('waiting');
+  sleep(1);
+}
+
+error_log('PARENT PROCESS : ' . file_get_contents($file));
 
 ?>
