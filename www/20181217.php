654edb85ed8bc1d2f760ad514d76387c064354e6
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-17 16:22:26 +0900

    Update 20181217.php

--- www/20181217.php
+++ www/20181217.php
@@ -12,7 +12,7 @@ $res = $mu->get_contents($url);
 
 $rc = preg_match('/LAST UPDATE (.+?)<.+?Free OCR API <span class="status {{ data.status }}">(.+?)<.+?Free OCR API <span class="status {{ data.status }}">(.+?)</s', $res, $matches);
 
-error_log(print_r($matches, TRUE));
+// error_log(print_r($matches, TRUE));
 error_log(trim($matches[2]) . ' ' . trim($matches[1]) . ' '. trim($matches[3]));
 
 if (trim($matches[2]) == 'DOWN') {
