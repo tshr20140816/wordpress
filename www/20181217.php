510d8138ad866df45356a53915b41700c3ddaa31
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-17 12:30:06 +0900

    Update 20181217.php

--- www/20181217.php
+++ www/20181217.php
@@ -12,7 +12,7 @@ $res = $mu->get_contents($matches[1]);
 
 $im = imagecreatefromstring($res);
 
-$im3 = imagecrop($im, ['x' => 0, 'y' => 95, 'width' => imagesx($im), 'height' => imagesy($im) - 145]);
+$im3 = imagecrop($im, ['x' => 30, 'y' => 95, 'width' => imagesx($im) - 60, 'height' => imagesy($im) - 145]);
 
 $canvas = imagecreatetruecolor(imagesx($im3) / 4, imagesy($im3) / 4);
 imagecopyresampled($canvas, $im3, 0, 0, 0, 0, imagesx($im3) / 4, imagesy($im3) / 4, imagesx($im3), imagesy($im3));
