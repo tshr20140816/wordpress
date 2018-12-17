f33e30ed82b974681b2cf853afce3dc13e195ac1
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-17 14:59:05 +0900

    Update 20181217.php

--- www/20181217.php
+++ www/20181217.php
@@ -4,7 +4,7 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $mu = new MyUtils();
 
-$url = 'https://weather.yahoo.co.jp/weather/jp/27/6200.html';
+$url = getenv('URL_KASA_SHISU');
 $res = $mu->get_contents($url);
 
 $rc = preg_match('/<!--指数情報-->.+?<span>傘指数(.+?)<.+?<p class="index_text">(.+?)</s', $res, $matches);
