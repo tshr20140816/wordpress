14619cbd4460a647e49bf048a6857e95128cf896
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-17 14:37:48 +0900

    Update 20181217.php

--- www/20181217.php
+++ www/20181217.php
@@ -9,6 +9,7 @@ $res = $mu->get_contents($url);
 
 $rc = preg_match('/<!--指数情報-->.+?<span>傘指数(.+?)<.+?<p class="index_text">(.+?)</s', $res, $matches);
 
+array_shift($matches);
 error_log(print_r($matches, TRUE));
 
 ?>
