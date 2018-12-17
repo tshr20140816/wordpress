cad9c85375221e0350a1f7b75acce956319369d6
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-17 09:21:27 +0900

    Update 20181216.php

--- www/20181216.php
+++ www/20181216.php
@@ -10,15 +10,14 @@ $res = $mu->get_contents($url);
 $rc = preg_match('/<p id="parkingnow"><img src="(.+?)"/s', $res, $matches);
 // $res = $mu->get_contents($matches[1]);
 
-/*
 $url = 'https://api.ocr.space/parse/imageurl?language=jpn&apikey=' . getenv('OCRSPACE_APIKEY') . '&url=' . $matches[1];
 
 $res = $mu->get_contents($url);
 
 $data = json_decode($res);
 error_log(print_r($data, TRUE));
-*/
 
+/*
 $res = $mu->get_contents($matches[1]);
 
 $json = ['requests' => [['image' => ['content' => base64_encode($res)],
@@ -36,4 +35,5 @@ $res = $mu->get_contents($url, $options);
 
 $data = json_decode($res);
 error_log(print_r($data, TRUE));
+*/
 ?>
