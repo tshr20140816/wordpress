29789833a5944713d175c55930a9b1146cf253d9
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-18 22:49:03 +0900

    Update x20181218.php

--- www/x20181218.php
+++ www/x20181218.php
@@ -1,3 +1,13 @@
 <?php
 
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$mu = new MyUtils();
+
+$url = 'http://www.motomachi-pa.jp/cgi/manku.pl?park_id=1&mode=pc';
+
+$res = $mu->get_contents($url);
+
+error_log(hash('sha512', $res));
+
 ?>
