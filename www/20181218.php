9581416a5d00b339608ff7d76ff823895f9c8051
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-18 09:59:46 +0900

    Update 20181218.php

--- www/20181218.php
+++ www/20181218.php
@@ -11,27 +11,27 @@ $mu = new MyUtils();
 $url = 'http://www.motomachi-pa.jp/cgi/manku.pl?park_id=1&mode=pc';
 $res = $mu->get_contents($url);
 
-error_log(md5($res));
+error_log(hash('sha512', $res));
 
 $url = 'http://www.motomachi-pa.jp/cgi/manku.pl?park_id=2&mode=pc';
 $res = $mu->get_contents($url);
 
-error_log(md5($res));
+error_log(hash('sha512', $res));
 
 $url = 'http://www.motomachi-pa.jp/cgi/manku.pl?park_id=3&mode=pc';
 $res = $mu->get_contents($url);
 
-error_log(md5($res));
+error_log(hash('sha512', $res));
 
 $url = 'http://www.motomachi-pa.jp/cgi/manku.pl?park_id=4&mode=pc';
 $res = $mu->get_contents($url);
 
-error_log(md5($res));
+error_log(hash('sha512', $res));
 
 $url = 'http://the-outlets-hiroshima.com/static/detail/car';
 $res = $mu->get_contents($url);
 $rc = preg_match('/<p id="parkingnow"><img src="(.+?)"/s', $res, $matches);
 $res = $mu->get_contents($matches[1]);
 
-error_log(md5($res));
+error_log(hash('sha512', $res));
 ?>
