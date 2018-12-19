719c1c8d124e4d2e941beaf77869e873c5f5adb4
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-19 09:20:45 +0900

    Update 20181219.php

--- www/20181219.php
+++ www/20181219.php
@@ -8,6 +8,8 @@ $res = $mu->get_contents($url);
 $rc = preg_match('/<p id="parkingnow"><img src="(.+?)"/s', $res, $matches);
 $res = $mu->get_contents($matches[1]);
 
-error_log($pid . ' NEW IMAGE (BASE64) : ' . base64_encode($res));
+error_log($pid . ' NEW IMAGE (BASE64) LENGTH : ' . strlen(base64_encode($res)));
+error_log($pid . ' NEW IMAGE (GZIP BASE64) LENGTH : ' . strlen(gzencode(base64_encode($res))));
+error_log($pid . ' NEW IMAGE (GZIP 9 BASE64) LENGTH : ' . strlen(gzencode(base64_encode($res), 9)));
 
 ?>
