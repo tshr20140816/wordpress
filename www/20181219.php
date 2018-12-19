eb60e356f7c6892756d197bea39e843f8349d4c4
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-19 13:08:35 +0900

    Update 20181219.php

--- www/20181219.php
+++ www/20181219.php
@@ -12,4 +12,10 @@ $rc = preg_match('/<!-- today index -->.+?<span class="indexes-telop-0">(.+?)<\/
 
 error_log(print_r($matches, TRUE));
 
+$res = $mu->get_contents(getenv('URL_KASA_SHISU2'));
+
+$rc = preg_match('/<!-- today index -->.+?<span class="indexes-telop-0">(.+?)<\/span>/s', $res, $matches);
+
+error_log(print_r($matches, TRUE));
+
 ?>
