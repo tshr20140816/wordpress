50381f762bc5bd561022d787bd47681d5af33a20
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-19 12:54:46 +0900

    Update 20181219.php

--- www/20181219.php
+++ www/20181219.php
@@ -8,6 +8,10 @@ $url = 'https://tenki.jp/indexes/self_temp/6/30/6200/';
 
 $res = $mu->get_contents($url);
 
-error_log($res);
+// error_log($res);
+
+$rc = preg_match('/<!-- today index -->(.+?)<!-- /today index -->/s', $res, $matches);
+
+error_log(print_r($matches, TRUE));
 
 ?>
