7a0e4260da8efed0e0a6e95ea403d66e2f31d6fe
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-20 10:55:00 +0900

    Update 20181219.php

--- www/20181219.php
+++ www/20181219.php
@@ -38,21 +38,23 @@ function get_shisu($mu_) {
   // $res = $mu_->get_contents(getenv('URL_TAIKAN_SHISU'));
   // $res = $mu_->get_contents(getenv('URL_KASA_SHISU'));
   
+  $list_shisu = [][];
+  
   foreach([getenv('URL_TAIKAN_SHISU'), getenv('URL_KASA_SHISU')] as $url) {
   
     $res = $mu_->get_contents($url);
     
     $rc = preg_match('/<!-- today index -->.+?<span class="indexes-telop-0">(.+?)<\/span>/s', $res, $matches);
-    $list[$url][$timestamp] = $matches[1];
+    $list_shisu[$url][$timestamp] = $matches[1];
   
     $rc = preg_match('/<!-- tomorrow index -->.+?<span class="indexes-telop-0">(.+?)<\/span>/s', $res, $matches);
-    $list[$url][$timestamp + 24 * 60 * 60] = $matches[1];
+    $list_shisu[$url][$timestamp + 24 * 60 * 60] = $matches[1];
   
     $rc = preg_match('/<!-- week -->(.+?)<!-- \/week -->/s', $res, $matches);
     $rc = preg_match_all('/<p class="indexes-telop-0">(.+?)<\/p>/s', $matches[1], $matches2, PREG_SET_ORDER);
   
     for($i = 0; $i < count($matches2); $i++) {
-      $list[$url][$timestamp + 24 * 60 * 60 * ($i + 2)] = $matches2[$i][1];
+      $list_shisu[$url][$timestamp + 24 * 60 * 60 * ($i + 2)] = $matches2[$i][1];
     }
   }
   return $list;
