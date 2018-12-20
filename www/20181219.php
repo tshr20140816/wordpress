5552d767c3d324182130956b083693447b3fac5d
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-20 13:25:04 +0900

    Update 20181219.php

--- www/20181219.php
+++ www/20181219.php
@@ -8,31 +8,29 @@ $list = get_shisu($mu);
 
 error_log(print_r($list, TRUE));
 
+$timestamp = 1545264000;
+
+if (array_key_exists($timestamp, $list_shisu[getenv('URL_KASA_SHISU')])) {
+  error_log('HIT');
+} else {
+  error_log('NONE');
+}
+
 function get_shisu($mu_) {
-  
-  $timestamp = strtotime('+9 hours');
-  
-  // $res = $mu_->get_contents(getenv('URL_TAIKAN_SHISU'));
-  // $res = $mu_->get_contents(getenv('URL_KASA_SHISU'));
-  
+  $timestamp = strtotime(date('j F Y', strtotime('+9 hours')));
   $list_shisu = [];
-  
   foreach([getenv('URL_TAIKAN_SHISU'), getenv('URL_KASA_SHISU')] as $url) {
-  
     $res = $mu_->get_contents($url);
-    
     $rc = preg_match('/<!-- today index -->.+?<span class="indexes-telop-0">(.+?)<\/span>/s', $res, $matches);
     $list_shisu[$url][$timestamp] = $matches[1];
-  
     $rc = preg_match('/<!-- tomorrow index -->.+?<span class="indexes-telop-0">(.+?)<\/span>/s', $res, $matches);
     $list_shisu[$url][$timestamp + 24 * 60 * 60] = $matches[1];
-  
     $rc = preg_match('/<!-- week -->(.+?)<!-- \/week -->/s', $res, $matches);
     $rc = preg_match_all('/<p class="indexes-telop-0">(.+?)<\/p>/s', $matches[1], $matches2, PREG_SET_ORDER);
-  
     for($i = 0; $i < count($matches2); $i++) {
       $list_shisu[$url][$timestamp + 24 * 60 * 60 * ($i + 2)] = $matches2[$i][1];
     }
+    error_log(getmypid() . ' [' . __METHOD__ . '] $list_shisu : ' . print_r($list_shisu, TRUE));
   }
   return $list_shisu;
 }
