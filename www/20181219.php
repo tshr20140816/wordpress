8a658136c0217bcc8649e9e9508a4dc650a5a459
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-20 10:57:08 +0900

    Update 20181219.php

--- www/20181219.php
+++ www/20181219.php
@@ -4,29 +4,6 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $mu = new MyUtils();
 
-$res = $mu->get_contents(getenv('URL_TAIKAN_SHISU'));
-
-// error_log($res);
-
-$rc = preg_match('/<!-- today index -->.+?<span class="indexes-telop-0">(.+?)<\/span>/s', $res, $matches);
-
-error_log(print_r($matches, TRUE));
-
-$res = $mu->get_contents(getenv('URL_KASA_SHISU'));
-
-$rc = preg_match('/<!-- today index -->.+?<span class="indexes-telop-0">(.+?)<\/span>/s', $res, $matches);
-
-error_log(print_r($matches, TRUE));
-
-$rc = preg_match('/<!-- tomorrow index -->.+?<span class="indexes-telop-0">(.+?)<\/span>/s', $res, $matches);
-
-error_log(print_r($matches, TRUE));
-
-$rc = preg_match('/<!-- week -->(.+?)<!-- \/week -->/s', $res, $matches);
-$rc = preg_match_all('/<p class="indexes-telop-0">(.+?)<\/p>/s', $matches[1], $matches2, PREG_SET_ORDER);
-
-error_log(print_r($matches2, TRUE));
-
 $list = get_shisu($mu);
 
 error_log(print_r($list, TRUE));
