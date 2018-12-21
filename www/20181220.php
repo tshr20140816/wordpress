6e581c2a2bf47e46e14bcc2edf4a3df62e3ffa2f
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-21 10:21:46 +0900

    Update 20181220.php

--- www/20181220.php
+++ www/20181220.php
@@ -68,5 +68,23 @@ DTEND;VALUE=DATE:__DTEND__
 END:VEVENT
 __HEREDOC__;
 
+$folder_id_label = $mu->get_folder_id('LABEL');
+$list_vevent = [];
+for ($i = 0; $i < count($tasks); $i++) {
+  if (array_key_exists('id', $tasks[$i])
+      && array_key_exists('folder', $tasks[$i])
+      && array_key_exists('dudate', $tasks[$i])
+     ) {
+    if ($folder_id_label == $tasks[$i]['folder']) {
+      continue;
+    }
+    $tmp = $template_vevent;
+    $tmp = str_replace('__SUMMARY__', $tasks[$i]['title'], $tmp);
+    $tmp = str_replace('__DTSTART__', date('Ymd', $tasks[$i]['dudate']), $tmp);
+    $tmp = str_replace('__DTEND__', date('Ymd', $tasks[$i]['dudate'] + 24 * 60 * 60), $tmp);
+    $list_vevent[] = $tmp;
+  }
+}
 
+error_log($pid . ' ' . count($list_vevent));
 ?>
