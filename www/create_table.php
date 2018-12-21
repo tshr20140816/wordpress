9caedd5b585fca5b7f8002ccaadf2a256240912d
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-21 16:49:36 +0900

    Update create_table.php

--- www/create_table.php
+++ www/create_table.php
@@ -2,9 +2,10 @@
 
 $connection_info = parse_url(getenv('DATABASE_URL'));
 $pdo = new PDO(
-  "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
-  $connection_info['user'],
-  $connection_info['pass']);
+    "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
+    $connection_info['user'],
+    $connection_info['pass']
+);
 
 $sql = <<< __HEREDOC__
 SELECT 'X'
@@ -16,7 +17,7 @@ $count = $pdo->exec($sql);
 error_log('m_authorization : ' . $count);
 
 if ($count == 0) {
-  $sql = <<< __HEREDOC__
+    $sql = <<< __HEREDOC__
 CREATE TABLE m_authorization (
   access_token character varying(255) NOT NULL
  ,expires_in bigint NOT NULL
@@ -26,8 +27,8 @@ CREATE TABLE m_authorization (
  ,update_time timestamp DEFAULT localtimestamp NOT NULL
 );
 __HEREDOC__;
-  $count = $pdo->exec($sql);
-  error_log('create table result : ' . $count);
+    $count = $pdo->exec($sql);
+    error_log('create table result : ' . $count);
 }
 
 $sql = <<< __HEREDOC__
@@ -40,15 +41,15 @@ $count = $pdo->exec($sql);
 error_log('t_webcache : ' . $count);
 
 if ($count == 0) {
-  $sql = <<< __HEREDOC__
+    $sql = <<< __HEREDOC__
 CREATE TABLE public.t_webcache (
     url_base64 character varying(1024) PRIMARY KEY,
     content_compress_base64 text,
     update_time timestamp without time zone DEFAULT LOCALTIMESTAMP NOT NULL
 );
 __HEREDOC__;
-  $count = $pdo->exec($sql);
-  error_log('create table result : ' . $count);
+    $count = $pdo->exec($sql);
+    error_log('create table result : ' . $count);
 }
 
 $sql = <<< __HEREDOC__
@@ -61,7 +62,7 @@ $count = $pdo->exec($sql);
 error_log('m_tenki : ' . $count);
 
 if ($count == 0) {
-  $sql = <<< __HEREDOC__
+    $sql = <<< __HEREDOC__
 CREATE TABLE public.m_tenki (
     location_number character varying(5) NOT NULL,
     point_name character varying(100) NOT NULL,
@@ -70,10 +71,8 @@ CREATE TABLE public.m_tenki (
 CREATE UNIQUE INDEX pk_m_tenki ON public.m_tenki USING btree (location_number, point_name, yyyymmdd);
 ALTER TABLE public.m_tenki CLUSTER ON pk_m_tenki;
 __HEREDOC__;
-  $count = $pdo->exec($sql);
-  error_log('create table result : ' . $count);
+    $count = $pdo->exec($sql);
+    error_log('create table result : ' . $count);
 }
 
 $pdo = null;
-
-?>
