518a5b929767584850b052929325dca6372ea545
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-21 11:54:28 +0900

    Update ical.php

--- www/ical.php
+++ www/ical.php
@@ -1,5 +1,20 @@
 <?php
 
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
+$file = '/tmp/toodledo_vcalendar.ics';
+
+header('Content-Type: text/calendar');
+if (file_exists($file)) {
+  $res = file_get_contents($file);
+  echo $res;
+} else {
+  echo "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nEND:VCALENDAR";
+}
+
+error_log("${pid} FINISH");
+exit();
 
 ?>
