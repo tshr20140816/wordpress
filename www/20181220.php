f2f487871b771310fc20eeb42a5e1dd010b61dca
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-21 09:41:33 +0900

    Update 20181220.php

--- www/20181220.php
+++ www/20181220.php
@@ -10,7 +10,17 @@ $access_token = $mu->get_access_token();
 $url = 'https://api.toodledo.com/3/tasks/get.php?comp=0&fields=tag,duedate,context,star,folder&access_token=' . $access_token;
 $res = $mu->get_contents($url);
 
-error_log($pid . ' TASKS (GZIP BASE64) : ' . strlen(gzencode(base64_encode($res), 9)));
 error_log($pid . ' TASKS (GZIP) : ' . strlen(gzencode($res, 9)));
+
+$pdo = $mu->get_pdo();
+
+$sql = "INSERT INTO t_test (data) VALUES ('" . gzencode($res, 9) . "')";
+
+$statement = $pdo->prepare($sql);
+$rc = $statement->execute();
+
+error_log($pid . ' ' . $rc);
+
+$pdo = null;
 
 ?>
