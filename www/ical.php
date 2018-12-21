0868849a6db43899c12a9ba20c6d3327b477d124
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-21 12:46:15 +0900

    Update ical.php

--- www/ical.php
+++ www/ical.php
@@ -5,15 +5,17 @@ $requesturi = $_SERVER['REQUEST_URI'];
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $ueragent = $_SERVER['HTTP_USER_AGENT'];
-error_log("${pid} START USER AGENT : ${ueragent}");
+error_log("${pid} USER AGENT : ${ueragent}");
 
 $file = '/tmp/toodledo_vcalendar.ics';
 
 header('Content-Type: text/calendar');
 if (file_exists($file) && $ueragent == getenv('USER_AGENT_ICS')) {
+  error_log("${pid} OK");
   $res = file_get_contents($file);
   echo $res;
 } else {
+  error_log("${pid} NG");
   echo "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nEND:VCALENDAR";
 }
 
