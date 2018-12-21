cb73cfc3d0aa83fd9edae611e6a8c2fd758cee50
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-21 16:13:49 +0900

    Update register.php

--- www/register.php
+++ www/register.php
@@ -7,10 +7,10 @@ $requesturi = $_SERVER['REQUEST_URI'];
 error_log("${pid} START ${requesturi}");
 
 if (!isset($_GET['code']) || !isset($_GET['state'])) {
-  $url = 'https://api.toodledo.com/3/account/authorize.php?response_type=code&client_id=' . getenv('TOODLEDO_CLIENTID') . '&state=' . uniqid() . '&scope=basic%20tasks%20notes%20write';
-  header('Location: ' . $url, TRUE, 301);
-  error_log("${pid} FINISH HTTP STATUS 301");
-  exit();
+    $url = 'https://api.toodledo.com/3/account/authorize.php?response_type=code&client_id=' . getenv('TOODLEDO_CLIENTID') . '&state=' . uniqid() . '&scope=basic%20tasks%20notes%20write';
+    header('Location: ' . $url, TRUE, 301);
+    error_log("${pid} FINISH HTTP STATUS 301");
+    exit();
 }
 
 $mu = new MyUtils();
@@ -24,11 +24,11 @@ error_log($pid . ' ' . $state . ' : ${state}');
 $post_data = ['grant_type' => 'authorization_code', 'code' => $code];
 
 $res = $mu->get_contents(
-  'https://api.toodledo.com/3/account/token.php',
-  [CURLOPT_USERPWD => getenv('TOODLEDO_CLIENTID') . ':' . getenv('TOODLEDO_SECRET'),
-   CURLOPT_POST => TRUE,
-   CURLOPT_POSTFIELDS => http_build_query($post_data),
-  ]);
+    'https://api.toodledo.com/3/account/token.php',
+    [CURLOPT_USERPWD => getenv('TOODLEDO_CLIENTID') . ':' . getenv('TOODLEDO_SECRET'),
+     CURLOPT_POST => TRUE,
+     CURLOPT_POSTFIELDS => http_build_query($post_data),
+    ]);
 
 error_log($pid . ' ' . $res . ' : ${res}');
 
@@ -36,9 +36,9 @@ $params = json_decode($res, TRUE);
 
 $connection_info = parse_url(getenv('DATABASE_URL'));
 $pdo = new PDO(
-  "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
-  $connection_info['user'],
-  $connection_info['pass']);
+    "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
+    $connection_info['user'],
+    $connection_info['pass']);
   
 $sql = 'TRUNCATE TABLE m_authorization;';
 
@@ -61,11 +61,11 @@ __HEREDOC__;
   
 $statement = $pdo->prepare($sql);
 $rc = $statement->execute(
-  [':b_access_token' => $params['access_token'],
-   ':b_expires_in' => $params['expires_in'],
-   ':b_refresh_token' => $params['refresh_token'],
-   ':b_scope' => $params['scope'],
-  ]);
+    [':b_access_token' => $params['access_token'],
+     ':b_expires_in' => $params['expires_in'],
+     ':b_refresh_token' => $params['refresh_token'],
+     ':b_scope' => $params['scope'],
+    ]);
 error_log("${pid} INSERT RESULT : ${rc}");
 
 $pdo = null;
