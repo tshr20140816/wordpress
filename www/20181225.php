e0233a0427a3a115859de2a88a05dfb25c7c3d6e
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-25 14:33:24 +0900

    Update 20181225.php

--- www/20181225.php
+++ www/20181225.php
@@ -10,3 +10,9 @@ $mu = new MyUtils();
 
 const LIST_WAFU_GETSUMEI = array('', '睦月', '如月', '弥生', '卯月', '皐月', '水無月', '文月', '葉月', '長月', '神無月', '霜月', '師走');
 
+for ($y = date('Y'); $y < date('Y') + 3; $y++) {
+    for ($m = 1; $m < 13; $m++) {
+        $timestamp = mktime(0, 0, 0, $m, 1, $y);
+        error_log($pid . ' ' . LIST_WAFU_GETSUMEI[$m] . ' ' . date('F', $timestamp));
+    }
+}
