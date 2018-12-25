dc893c019e2caa8f401c1ee25847bd91ab03adf5
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-25 09:08:11 +0900

    Update create_table.php

--- www/create_table.php
+++ www/create_table.php
@@ -7,17 +7,8 @@ $pdo = new PDO(
     $connection_info['pass']
 );
 
-$sql = <<< __HEREDOC__
-SELECT 'X'
-  FROM pg_class V1
- WHERE V1.relkind = 'r'
-   AND V1.relname = 'm_authorization';
-__HEREDOC__;
-$count = $pdo->exec($sql);
-error_log('m_authorization : ' . $count);
 
-if ($count == 0) {
-    $sql = <<< __HEREDOC__
+$sql = <<< __HEREDOC__
 CREATE TABLE m_authorization (
   access_token character varying(255) NOT NULL
  ,expires_in bigint NOT NULL
@@ -27,52 +18,47 @@ CREATE TABLE m_authorization (
  ,update_time timestamp DEFAULT localtimestamp NOT NULL
 );
 __HEREDOC__;
-    $count = $pdo->exec($sql);
-    error_log('create table result : ' . $count);
-}
+$count = $pdo->exec($sql);
+error_log('create table result : ' . $count);
 
 $sql = <<< __HEREDOC__
-SELECT 'X'
-  FROM pg_class V1
- WHERE V1.relkind = 'r'
-   AND V1.relname = 't_webcache';
-__HEREDOC__;
-$count = $pdo->exec($sql);
-error_log('t_webcache : ' . $count);
-
-if ($count == 0) {
-    $sql = <<< __HEREDOC__
 CREATE TABLE public.t_webcache (
     url_base64 character varying(1024) PRIMARY KEY,
     content_compress_base64 text,
     update_time timestamp without time zone DEFAULT LOCALTIMESTAMP NOT NULL
 );
 __HEREDOC__;
-    $count = $pdo->exec($sql);
-    error_log('create table result : ' . $count);
-}
+$count = $pdo->exec($sql);
+error_log('create table result : ' . $count);
 
 $sql = <<< __HEREDOC__
-SELECT 'X'
-  FROM pg_class V1
- WHERE V1.relkind = 'r'
-   AND V1.relname = 'm_tenki';
-__HEREDOC__;
-$count = $pdo->exec($sql);
-error_log('m_tenki : ' . $count);
-
-if ($count == 0) {
-    $sql = <<< __HEREDOC__
 CREATE TABLE public.m_tenki (
     location_number character varying(5) NOT NULL,
     point_name character varying(100) NOT NULL,
-    yyyymmdd character varying(8) NOT NULL
+    yyyymmdd character varying(8) NOT NULL,
+    PRIMARY KEY (location_number, point_name, yyyymmdd)
 );
-CREATE UNIQUE INDEX pk_m_tenki ON public.m_tenki USING btree (location_number, point_name, yyyymmdd);
-ALTER TABLE public.m_tenki CLUSTER ON pk_m_tenki;
 __HEREDOC__;
-    $count = $pdo->exec($sql);
-    error_log('create table result : ' . $count);
-}
+$count = $pdo->exec($sql);
+error_log('create table result : ' . $count);
+
+$sql = <<< __HEREDOC__
+CREATE TABLE t_ical (
+    ical_data text
+);
+__HEREDOC__;
+$count = $pdo->exec($sql);
+error_log('create table result : ' . $count);
+
+$sql = <<< __HEREDOC__
+CREATE TABLE public.t_imageparsehash (
+    group_id integer NOT NULL,
+    hash_text character varying(128) NOT NULL,
+    parse_text text NOT NULL,
+    update_time timestamp without time zone DEFAULT LOCALTIMESTAMP NOT NULL,
+    PRIMARY KEY (group_id, hash_text)
+);
+$count = $pdo->exec($sql);
+error_log('create table result : ' . $count);
 
 $pdo = null;
