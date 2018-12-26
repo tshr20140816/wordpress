ef0d09dc6ab06af2df12253b475dc773674917e5
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-26 12:43:42 +0900

    Update 20181226.php

--- www/20181226.php
+++ www/20181226.php
@@ -13,13 +13,9 @@ $url = 'http://i.river.go.jp/_-p02-_/p/ktm1201010/?mtm=0&swd=&rvr=87712001&prf=3
 $res = $mu->get_contents($url);
 
 $res = mb_convert_encoding($res, 'UTF-8', 'SJIS');
-
 // error_log($res);
 
 $tmp = explode('<hr/>', $res)[2];
-
-// error_log($tmp);
-
 $tmp = trim(str_replace('  ', ' ', strip_tags(str_replace('&nbsp;', '', $tmp))));
 $tmp = str_replace('の更新情報', '', $tmp);
 $tmp = str_replace('単位：m ■', '', $tmp);
@@ -29,5 +25,8 @@ error_log($tmp);
 $url = 'http://www.river.go.jp/kawabou/ipSuiiKobetu.do?obsrvId=0716900400013&gamenId=01-1003&stgGrpKind=survForeKjExpl&fldCtlParty=no&fvrt=yes&timeType=10';
 
 $res = $mu->get_contents($url);
+// error_log($res);
 
-error_log($res);
+$tmp = explode('<div id="hyou" style="width:278px; height:390px; overflow-y:auto;">', $res)[1];
+$tmp = explode('</table">', $tmp)[0];
+error_log($tmp);
