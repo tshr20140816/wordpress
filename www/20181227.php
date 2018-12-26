efdd9732aed5e3ffe20c7a8e285b8e797fe22b67
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-27 06:55:33 +0900

    Update 20181227.php

--- www/20181227.php
+++ www/20181227.php
@@ -3,6 +3,8 @@
 $url = 'https://api.dropboxapi.com/2/file_requests/list';
 
 /*
+https://www.dropbox.com/developers/documentation/http/documentation
+
 curl -X POST "https://api.dropbox.com/1/metadata/link" \
     --header "Authorization: Basic <base64(APP_KEY:APP_SECRET)>" \
     -d "link=https://www.dropbox.com/sh/748f94925f0gesq/AAAMSoRJyhJFfkupnAU0wXuva?dl=0"
