874707259d938f81777f1b280964790eba457b44
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-26 09:07:27 +0900

    Update 20181226.php

--- www/20181226.php
+++ www/20181226.php
@@ -8,11 +8,20 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
+$y = 2018;
+$m = 12;
+$d = 26;
+
 $url = 'https://keisan.casio.jp/exec/system/1186108192';
 
-$post_data = ['lang' => '', 'charset' => 'utf-8', 'var_c1' => '0', 'var_Y' => '2018', 'var_M' => '12', 'var_D' => '26',];
+$post_data = ['lang' => '', 'charset' => 'utf-8', 'var_c1' => '0', 'var_Y' => $y, 'var_M' => $m, 'var_D' => $d,];
 
 $res = $mu->get_contents($url, [CURLOPT_POST => true, CURLOPT_POSTFIELDS => http_build_query($post_data),]);
 
 error_log($res);
 
+$url = "http://api.sekido.info/qreki?output=json&year=${y}&month=${m}&day=${d}";
+
+$res = $mu->get_contents($url);
+
+error_log(json_decode($res, true));
