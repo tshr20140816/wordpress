2d831901325dca2390d2447b5fa941381358f86e
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-26 15:28:43 +0900

    Update 20181226.php

--- www/20181226.php
+++ www/20181226.php
@@ -6,6 +6,8 @@ $pid = getmypid();
 $requesturi = $_SERVER['REQUEST_URI'];
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
+$rc = apcu_clear_cache();
+
 $mu = new MyUtils();
 
 $url = $mu->get_env('URL_OUTLET');
