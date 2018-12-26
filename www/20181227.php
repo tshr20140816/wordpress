384917e3dd67e94afc17c523ccc88b3a94c2ce5d
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-27 06:54:23 +0900

    Create 20181227.php

---
+++ www/20181227.php
@@ -0,0 +1,9 @@
+<?php
+
+$url = 'https://api.dropboxapi.com/2/file_requests/list';
+
+/*
+curl -X POST "https://api.dropbox.com/1/metadata/link" \
+    --header "Authorization: Basic <base64(APP_KEY:APP_SECRET)>" \
+    -d "link=https://www.dropbox.com/sh/748f94925f0gesq/AAAMSoRJyhJFfkupnAU0wXuva?dl=0"
+*/
