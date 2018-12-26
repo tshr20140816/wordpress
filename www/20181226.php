0012f6a9d5fbb02d495775237fe337e4ce592d2c
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-26 15:28:08 +0900

    Create 20181226.php

---
+++ www/20181226.php
@@ -0,0 +1,11 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+$url = $mu->get_env('URL_OUTLET');
