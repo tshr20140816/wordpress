ec4bccd6faa753711fbf12c38a6b9723007f7f2e
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-26 13:33:28 +0900

    Update 20181226.php

--- www/20181226.php
+++ www/20181226.php
@@ -8,31 +8,16 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-/*
-$url = getenv('URL_RIVER_1');
-$res = $mu->get_contents($url);
-$res = mb_convert_encoding($res, 'UTF-8', 'SJIS');
-// error_log($res);
-
-$tmp = explode('<hr/>', $res)[2];
-$tmp = trim(str_replace('  ', ' ', strip_tags(str_replace('&nbsp;', '', $tmp))));
-$tmp = str_replace('の更新情報', '', $tmp);
-$tmp = str_replace('単位：m ■', '', $tmp);
-$tmp = str_replace('(自)', ' ', $tmp) . 'm';
-error_log($tmp);
-*/
-
-$url = getenv('URL_RIVER_2');
-
+foreach ([getenv('URL_RIVER_1'), getenv('URL_RIVER_2')] as $url) {
 $res = $mu->get_contents($url);
 // error_log($res);
 
 $rc = preg_match('/観測所：(.+?)\(/s', $res, $matches);
-error_log(print_r($matches, true));
+// error_log(print_r($matches, true));
 $point_name = $matches[1];
 
 $rc = preg_match('/雨量観測所<\/th>.+?<td.+?>.+?<td.+?>(.+?)</s', $res, $matches);
-error_log(print_r($matches, true));
+// error_log(print_r($matches, true));
 $river_name = trim($matches[1]);
 
 $tmp = explode('<div id="hyou" style="width:278px; height:390px; overflow-y:auto;">', $res)[1];
@@ -41,3 +26,4 @@ $tmp = explode('</table>', $tmp)[0];
 $rc = preg_match('/.+<tr.+?>.+?<td.+?>(.+?)<\/td>.+?<td.+?>(.+?)</s', $tmp, $matches);
 // error_log(print_r($matches, true));
 error_log(trim($matches[1]) . " ${river_name} ${point_name} " . trim($matches[2]) . 'm');
+}
