b15714a1310c0a83b6e3a38346f8dd8893af8dd4
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-26 11:41:49 +0900

    Update 20181226.php

--- www/20181226.php
+++ www/20181226.php
@@ -8,43 +8,8 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-$y = 2018;
-$m = 12;
-$d = 31;
+$url = 'http://www.kasen-bousai.pref.hiroshima.lg.jp/rivercontents/p10202/10/31_1.html';
 
-$url = 'https://keisan.casio.jp/exec/system/1186108192';
-
-$post_data = ['lang' => '', 'charset' => 'utf-8', 'var_c1' => '0', 'var_Y' => $y, 'var_M' => $m, 'var_D' => $d,];
-
-$res = $mu->get_contents($url, [CURLOPT_POST => true, CURLOPT_POSTFIELDS => http_build_query($post_data),]);
+$res = $mu->get_contents($url);
 
 error_log($res);
-
-$url = "http://api.sekido.info/qreki?output=json&year=${y}&month=${m}&day=${d}";
-
-$res = $mu->get_contents($url);
-
-error_log(print_r(json_decode($res, true), true));
-
-$url = "https://dateinfoapi.appspot.com/v1?date=${y}-${m}-${d}";
-
-$res = $mu->get_contents($url);
-
-error_log(print_r(json_decode($res, true), true));
-
-$url = 'http://koyomi.zing2.org/api/';
-
-$post_data = ['mode' => 'd', 'cnt' => '1', 'targetyyyy' => $y, 'targetmm' => $m, 'targetdd' => $d,];
-
-$res = $mu->get_contents($url, [CURLOPT_POST => true, CURLOPT_POSTFIELDS => http_build_query($post_data),]);
-
-error_log(print_r(json_decode($res, true), true));
-
-$url = 'http://koyomi.zing2.org/api/';
-
-$post_data = ['mode' => 'm', 'cnt' => '12', 'targetyyyy' => $y, 'targetmm' => $m];
-
-$res = $mu->get_contents($url, [CURLOPT_POST => true, CURLOPT_POSTFIELDS => http_build_query($post_data),]);
-
-error_log(print_r(json_decode($res, true), true));
-
