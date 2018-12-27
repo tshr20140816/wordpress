b079495a1c599cef0a018fdd94bc8a4506fec998
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-27 15:28:59 +0900

    Update hourly.php

--- www/hourly.php
+++ www/hourly.php
@@ -853,7 +853,7 @@ __HEREDOC__;
     }
     $list_vevent[] = $vevent_footer;
 
-    error_log(getmypid() . ' VEVENT COUNT : ' . count($list_vevent));
+    error_log(getmypid() . ' [' . __METHOD__ . '] VEVENT COUNT : ' . count($list_vevent));
 
     $ical_data = implode("\r\n", $list_vevent);
 
@@ -862,12 +862,12 @@ __HEREDOC__;
     $sql = 'TRUNCATE TABLE t_ical';
     $statement = $pdo->prepare($sql);
     $rc = $statement->execute();
-    error_log(getmypid() . ' TRUNCATE $rc : ' . $rc);
+    error_log(getmypid() . ' [' . __METHOD__ . '] TRUNCATE $rc : ' . $rc);
 
     $sql = 'INSERT INTO t_ical (ical_data) VALUES (:b_ical_data)';
     $statement = $pdo->prepare($sql);
     $rc = $statement->execute([':b_ical_data' => base64_encode(gzencode($ical_data, 9))]);
-    error_log(getmypid() . ' INSERT $rc : ' . $rc);
+    error_log(getmypid() . ' [' . __METHOD__ . '] INSERT $rc : ' . $rc);
 
     $pdo = null;
 }
