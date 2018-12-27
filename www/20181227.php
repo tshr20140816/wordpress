1c03cf79f9f188a52efd8eb611d07c9a3c433f22
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-27 11:39:20 +0900

    Update 20181227.php

--- www/20181227.php
+++ www/20181227.php
@@ -3,5 +3,15 @@
 error_log(getmypid() . ' START');
 
 $pid = pcntl_fork();
+if ($pid == -1) {
+     error_log('fork できません');
+} else if ($pid) {
+     // 親プロセスの場合
+    error_log('parent process');
+    pcntl_wait($status); // ゾンビプロセスから守る
+} else {
+    // 子プロセスの場合
+    error_log('child process');
+}
 
 error_log(getmypid() . ' FINISH');
