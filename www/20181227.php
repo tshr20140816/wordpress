6fcb08b02a56f4ffc429ffebbc4fb8262c44f8b6
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-27 13:42:52 +0900

    Update 20181227.php

--- www/20181227.php
+++ www/20181227.php
@@ -8,7 +8,7 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-$timeout = 10;
+$timeout = 5;
 
 $mh = curl_multi_init();
 
@@ -16,17 +16,18 @@ $urls = [$mu->get_env('URL_KASA_SHISU_YAHOO'), $mu->get_env('URL_WEATHER_WARN')]
 
 error_log(print_r($urls, true));
 
-foreach ($urls as $u) {
+$ch = [];
+foreach ($urls as $url) {
     error_log('POINT 100');
-    $ch = curl_init();
-    curl_setopt_array($ch, array(
-        CURLOPT_URL            => $u,
+    $ch[$url] = curl_init();
+    curl_setopt_array($ch[$url], array(
+        CURLOPT_URL            => $url,
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_TIMEOUT        => $timeout,
         CURLOPT_CONNECTTIMEOUT => $timeout,
         CURLOPT_USERAGENT => getenv('USER_AGENT'),
     ));
-    curl_multi_add_handle($mh, $ch);
+    curl_multi_add_handle($mh, $ch[$url]);
     error_log('POINT 110');
 }
 
@@ -36,7 +37,6 @@ do {
     error_log('POINT 130');
 } while ($stat === CURLM_CALL_MULTI_PERFORM);
 if ( ! $running || $stat !== CURLM_OK) {
-    // throw new RuntimeException('リクエストが開始出来なかった。マルチリクエスト内のどれか、URLの設定がおかしいのでは？');
     error_log('POINT 140');
     exit();
 }
@@ -56,8 +56,15 @@ while ($active && $mrc == CURLM_OK) {
     do {
         $mrc = curl_multi_exec($mh, $active);
     } while ($mrc == CURLM_CALL_MULTI_PERFORM);
-    error_log('POINT 160');
+    // error_log('POINT 160');
 }
+
+foreach ($urls as $url) {
+    $results[$url] = curl_getinfo($ch[$url]);
+    curl_multi_remove_handle($mh, $ch[$url]);
+    curl_close($ch[$url]);
+}
+error_log(print_r($results, true)):
 
 curl_multi_close($mh);
 
