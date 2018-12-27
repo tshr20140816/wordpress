9ea1808db9b1e3ddad4f62396be15c23ae2f8ede
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-27 11:54:01 +0900

    Update 20181227.php

--- www/20181227.php
+++ www/20181227.php
@@ -8,11 +8,13 @@ if ($pid == -1) {
 } else if ($pid) {
     // 親プロセスの場合
     error_log('parent process');
+    sleep(3);
     pcntl_wait($status); // ゾンビプロセスから守る
     error_log('$status : ' . $status);
 } else {
     // 子プロセスの場合
     error_log('child process');
+    sleep(3);
 }
 
 error_log(getmypid() . ' FINISH');
