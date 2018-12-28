dead4cc5bf98303bb05138ad179b55ceae0638d1
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-12-28 09:17:02 +0900

    Update MyUtils.php

--- classes/MyUtils.php
+++ classes/MyUtils.php
@@ -481,7 +481,7 @@ __HEREDOC__;
             } while ($rc == CURLM_CALL_MULTI_PERFORM);
             */
             $rc = curl_multi_exec($mh, $active);
-            error_log($rc);
+            error_log('CHECK POINT 300 : ' . $rc);
             sleep(1);
         }
 
