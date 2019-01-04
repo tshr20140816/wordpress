341d68f3be631841167fbf06a15f8453bdbeb68d
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-04 17:16:43 +0900

    Create daily_pre.php

---
+++ www/daily_pre.php
@@ -0,0 +1,22 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = time();
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s', $time_start));
+
+$rc = apcu_clear_cache();
+
+$mu = new MyUtils();
+
+$sub_address = $mu->get_env('SUB_ADDRESS');
+for ($i = 0; $i < 12; $i++) {
+    $url = 'https://feed43.com/' . $sub_address . ($i * 5 + 11) . '-' . ($i * 5 + 15) . '.xml';
+    $res = $mu->get_contents($url);
+}
+
+$time_finish = time();
+error_log("${pid} FINISH " . date('s', $time_finish - $time_start) . 's');
+exit();
