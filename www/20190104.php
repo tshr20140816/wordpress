fdc1e577a2f25b024b314c2ce6690a5984e8ca0c
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-04 14:36:06 +0900

    Update 20190104.php

--- www/20190104.php
+++ www/20190104.php
@@ -13,15 +13,13 @@ $rc = func001($mu);
 
 function func001($mu_) {
     
-    // cache search on
-    
+    // cache search on list    
     $urls_is_cache['https://api.heroku.com/account'] =
         [CURLOPT_HTTPHEADER => ['Accept: application/vnd.heroku+json; version=3',
                                 'Authorization: Bearer ' . getenv('HEROKU_API_KEY'),
                                ]];
 
-    // cache search off
-    
+    // cache search off list    
     $urls[$mu_->get_env('URL_AMEDAS')] = null;
     
     $sql = <<< __HEREDOC__
@@ -55,4 +53,51 @@ __HEREDOC__;
     }
     
     $mh = curl_multi_init();
+    
+    foreach ($urls in $url => $options_add) {
+        error_log('CURL ADD URL : ' . $url);
+        $ch = curl_init();
+        $options = [CURLOPT_URL => $url,
+                    CURLOPT_USERAGENT => getenv('USER_AGENT'),
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_FOLLOWLOCATION => 1,
+                    CURLOPT_MAXREDIRS => 3,
+                    CURLOPT_SSL_FALSESTART => true,
+        ];
+        curl_setopt_array($ch, $options);
+        if (is_null($options_add) == false) {
+            curl_setopt_array($ch, $options_add);
+        }
+        curl_multi_add_handle($mh, $ch);
+        $list_ch[$url] = $ch;
+    }
+    
+    $active = null;
+    $rc = curl_multi_exec($mh, $active);
+    
+    while ($active && $rc == CURLM_OK) {
+        if (curl_multi_select($mh) == -1) {
+            usleep(1);
+        }
+        $rc = curl_multi_exec($mh, $active);
+    }
+    
+    foreach (array_keys($urls) as $url) {
+        $ch = $list_ch[$url];
+        $res = curl_getinfo($ch);
+        if ($res['http_code'] == 200) {
+            $result = curl_multi_getcontent($ch);
+            $results[$url] = $result;
+        }
+        curl_multi_remove_handle($mh, $ch);
+        curl_close($ch);
+        // error_log(print_r($res, true));
+    }
+    
+    curl_multi_close($mh);
+ 
+    foreach ($results as $url => $result) {
+        error_log('CURL RESULT URL : ' . $url);
+    }
 }
