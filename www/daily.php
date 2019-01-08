56dfd165e400dd571aec50238c595018959463ca
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-08 09:12:09 +0900

    Update daily.php

--- www/daily.php
+++ www/daily.php
@@ -491,11 +491,20 @@ function get_task_highway($mu_)
     // Get Contexts
     $list_context_id = $mu_->get_contexts();
 
+    /*
     $url = 'https://www.w-nexco.co.jp/traffic_info/construction/traffic.php?fdate='
       . date('Ymd', strtotime('+1 day'))
       . '&tdate='
       . date('Ymd', strtotime('+14 day'))
       . '&ak=1&ac=1&kisei%5B%5D=901&dirc%5B%5D=1&dirc%5B%5D=2&order=2&ronarrow=1'
+      . '&road%5B%5D=1011&road%5B%5D=1912&road%5B%5D=1020&road%5B%5D=225A&road%5B%5D=1201'
+      . '&road%5B%5D=1222&road%5B%5D=1231&road%5B%5D=234D&road%5B%5D=1232&road%5B%5D=1260';
+    */
+    $url = 'https://www.w-nexco.co.jp/traffic_info/construction/traffic.php?fdate='
+      . date('Ymd', strtotime('+1 day'))
+      . '&tdate='
+      . date('Ymd', strtotime('+14 day'))
+      . '&ak=1&ac=1&kisei%5B%5D=901&dirc%5B%5D=1&dirc%5B%5D=2&order=2&ronarrow=0'
       . '&road%5B%5D=1011&road%5B%5D=1912&road%5B%5D=1020&road%5B%5D=225A&road%5B%5D=1201'
       . '&road%5B%5D=1222&road%5B%5D=1231&road%5B%5D=234D&road%5B%5D=1232&road%5B%5D=1260';
 
