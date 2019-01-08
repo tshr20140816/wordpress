179282491d083407df2d52b554eaf5e9f612396f
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-08 09:14:23 +0900

    Update daily_pre.php

--- www/daily_pre.php
+++ www/daily_pre.php
@@ -13,6 +13,17 @@ $mu = new MyUtils();
 
 //
 
+$url = 'https://www.w-nexco.co.jp/traffic_info/construction/traffic.php?fdate='
+    . date('Ymd', strtotime('+1 day'))
+    . '&tdate='
+    . date('Ymd', strtotime('+14 day'))
+    . '&ak=1&ac=1&kisei%5B%5D=901&dirc%5B%5D=1&dirc%5B%5D=2&order=2&ronarrow=0'
+    . '&road%5B%5D=1011&road%5B%5D=1912&road%5B%5D=1020&road%5B%5D=225A&road%5B%5D=1201'
+    . '&road%5B%5D=1222&road%5B%5D=1231&road%5B%5D=234D&road%5B%5D=1232&road%5B%5D=1260';
+$res = $mu->get_contents($url, null, true);
+
+//
+
 for ($j = 0; $j < 4; $j++) {
     $yyyy = date('Y', strtotime('+' . $j . ' years'));
 
