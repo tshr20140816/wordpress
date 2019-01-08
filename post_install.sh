f3705d94e65332eb8ecc1f41e0d5e3dcef37d82b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-08 16:48:38 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -12,12 +12,18 @@ cp ../config.inc.php phppgadmin/conf/
 cp ../Connection.php phppgadmin/classes/database/
 popd
 
-pushd /temp
+pushd /tmp
 
 wget https://ja.wordpress.org/wordpress-5.0.2-ja.tar.gz
 tar xf wordpress-5.0.2-ja.tar.gz
 
 ls -lang
+
+pushd wordpress
+
+ls -lang
+
+popd
 
 popd
 
