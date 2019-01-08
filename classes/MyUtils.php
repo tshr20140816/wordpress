1f422d973c7341e6029f68710d85899d108cd83f
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-08 09:29:41 +0900

    Update MyUtils.php

--- classes/MyUtils.php
+++ classes/MyUtils.php
@@ -133,7 +133,8 @@ __HEREDOC__;
         $file_name = '/tmp/contexts';
         if (file_exists($file_name)) {
             $list_context_id = unserialize(file_get_contents($file_name));
-            error_log($log_prefix . '(CACHE HIT) $list_context_id : ' . print_r($list_context_id, true));
+            // error_log($log_prefix . '(CACHE HIT) $list_context_id : ' . print_r($list_context_id, true));
+            error_log($log_prefix . '(CACHE HIT) $list_context_id';
             return $list_context_id;
         }
 
