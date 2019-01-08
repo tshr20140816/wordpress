e4c07d5c15f3f4969ed594e6f8c4defb4d4c3b09
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-08 17:07:45 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -12,19 +12,15 @@ cp ../config.inc.php phppgadmin/conf/
 cp ../Connection.php phppgadmin/classes/database/
 popd
 
-pushd /tmp
+# wget https://ja.wordpress.org/wordpress-5.0.2-ja.tar.gz
+# tar xf wordpress-5.0.2-ja.tar.gz
 
-wget https://ja.wordpress.org/wordpress-5.0.2-ja.tar.gz
-tar xf wordpress-5.0.2-ja.tar.gz
+pushd tmp
 
-ls -lang
+git clone --depth 1 https://github.com/mhoofman/wordpress-heroku.git wordpress
 
 pushd wordpress
-
 ls -lang
-
-cat wp-config-sample.php
-
 popd
 popd
 
