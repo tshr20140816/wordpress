d3241c6600226c83cc1e43234cff80d456a891df
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-08 10:08:55 +0900

    Update outlet_parking_information.php

--- www/outlet_parking_information.php
+++ www/outlet_parking_information.php
@@ -4,6 +4,7 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $pid = getmypid();
 $requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
@@ -135,4 +136,5 @@ __HEREDOC__;
   
     $pdo = null;
 }
-error_log("${pid} FINISH");
+$time_finish = microtime(true);
+error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's');
