117de902cd58a02a16ef693ef358cbe42e66d3c2
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-08 09:39:36 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -12,8 +12,8 @@ cp ../config.inc.php phppgadmin/conf/
 cp ../Connection.php phppgadmin/classes/database/
 popd
 
-wget https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.0/phpcs.phar
-wget https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.0/phpcbf.phar
+wget -q https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.0/phpcs.phar
+wget -q https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.0/phpcbf.phar
 
 chmod 755 ./start_web.sh
 
